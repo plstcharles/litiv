c64f18aaef347e895d6ca1166e7d6dbae2cc985a
Pierre-Luc St-Charles <pierre-luc.st-charles@polymtl.ca>
2013-06-14 17:27:42 -0400

    Self-note: L2 dist gives awful results when used on the last step of the BGS algorithm.

--- BackgroundSubtractorLBSP.cpp
+++ BackgroundSubtractorLBSP.cpp
@@ -126,14 +126,14 @@ void BackgroundSubtractorLBSP::operator()(cv::InputArray _image, cv::OutputArray
 		// note: modulation is commented here since noise doesn't affect the pattern that much, considering we use the modulated color threshold to build it
 		const int nCurrDescDistThreshold = m_nDescDistThreshold/**LBSP_SINGLECHANNEL_THRESHOLD_MODULATION_FACT*/;
 		const int nCurrColorDistThreshold = m_nColorDistThreshold*LBSP_SINGLECHANNEL_THRESHOLD_MODULATION_FACT;
-		unsigned short inputdesc;
+		unsigned short nCurrInputDesc;
 		for(int k=0; k<nKeyPoints; ++k) {
 			const int x = (int)m_voKeyPoints[k].pt.x;
 			const int y = (int)m_voKeyPoints[k].pt.y;
 			int nGoodSamplesCount=0, nSampleIdx=0;
 			while(nGoodSamplesCount<m_nRequiredBGSamples && nSampleIdx<m_nBGSamples) {
-				LBSP::computeSingle(oInputImg,m_voBGImg[nSampleIdx],x,y,nCurrColorDistThreshold,inputdesc);
-				if(hdist_ushort_8bitLUT(inputdesc,m_voBGDesc[nSampleIdx].at<unsigned short>(y,x))<=nCurrDescDistThreshold) {
+				LBSP::computeSingle(oInputImg,m_voBGImg[nSampleIdx],x,y,nCurrColorDistThreshold,nCurrInputDesc);
+				if(hdist_ushort_8bitLUT(nCurrInputDesc,m_voBGDesc[nSampleIdx].at<unsigned short>(y,x))<=nCurrDescDistThreshold) {
 					const int idx_img = oInputImg.step.p[0]*y + x;
 					CV_DbgAssert(*(oInputImg.data+idx_img)==oInputImg.at<uchar>(y,x));
 					if(absdiff_uchar(*(oInputImg.data+idx_img),*(m_voBGImg[nSampleIdx].data+idx_img))<=nCurrColorDistThreshold)
@@ -146,16 +146,16 @@ void BackgroundSubtractorLBSP::operator()(cv::InputArray _image, cv::OutputArray
 			else {
 				if((rand()%nLearningRate)==0) {
 					int s_rand = rand()%m_nBGSamples;
-					LBSP::computeSingle(oInputImg,cv::Mat(),x,y,nCurrColorDistThreshold,inputdesc);
-					m_voBGDesc[s_rand].at<unsigned short>(y,x) = inputdesc;
+					LBSP::computeSingle(oInputImg,cv::Mat(),x,y,nCurrColorDistThreshold,nCurrInputDesc);
+					m_voBGDesc[s_rand].at<unsigned short>(y,x) = nCurrInputDesc;
 					m_voBGImg[s_rand].at<uchar>(y,x) = oInputImg.at<uchar>(y,x);
 				}
 				if((rand()%nLearningRate)==0) {
 					int s_rand = rand()%m_nBGSamples;
 					int x_rand,y_rand;
 					getRandNeighborPosition(x_rand,y_rand,x,y,LBSP::DESC_SIZE/2,m_oImgSize);
-					LBSP::computeSingle(oInputImg,cv::Mat(),x,y,nCurrColorDistThreshold,inputdesc);
-					m_voBGDesc[s_rand].at<unsigned short>(y_rand,x_rand) = inputdesc;
+					LBSP::computeSingle(oInputImg,cv::Mat(),x,y,nCurrColorDistThreshold,nCurrInputDesc);
+					m_voBGDesc[s_rand].at<unsigned short>(y_rand,x_rand) = nCurrInputDesc;
 					m_voBGImg[s_rand].at<uchar>(y_rand,x_rand) = oInputImg.at<uchar>(y,x);
 				}
 			}
@@ -166,8 +166,8 @@ void BackgroundSubtractorLBSP::operator()(cv::InputArray _image, cv::OutputArray
 		const int nCurrColorDistThreshold = m_nColorDistThreshold*3;
 		const int nCurrSCDescDistThreshold = m_nDescDistThreshold*BGSLBSP_SINGLECHANNEL_THRESHOLD_DIFF_FACTOR;
 		const int nCurrSCColorDistThreshold = m_nColorDistThreshold*BGSLBSP_SINGLECHANNEL_THRESHOLD_DIFF_FACTOR;
-		unsigned short inputdesc[3];
-		int hdist[3], l1dist[3];
+		unsigned short anCurrInputDesc[3];
+		int anDescDist[3], anColorDist[3];
 		const int desc_row_step = m_voBGDesc[0].step.p[0];
 		const int img_row_step = m_voBGImg[0].step.p[0];
 		CV_DbgAssert(m_voBGDesc[0].step.p[1]==6 && m_voBGImg[0].step.p[1]==3);
@@ -178,20 +178,19 @@ void BackgroundSubtractorLBSP::operator()(cv::InputArray _image, cv::OutputArray
 			const int idx_img = img_row_step*y + 3*x;
 			int nGoodSamplesCount=0, nSampleIdx=0;
 			while(nGoodSamplesCount<m_nRequiredBGSamples && nSampleIdx<m_nBGSamples) {
-				LBSP::computeSingle(oInputImg,m_voBGImg[nSampleIdx],x,y,m_nColorDistThreshold,inputdesc);
+				LBSP::computeSingle(oInputImg,m_voBGImg[nSampleIdx],x,y,m_nColorDistThreshold,anCurrInputDesc);
 				const unsigned short* bgdesc_ptr = (unsigned short*)(m_voBGDesc[nSampleIdx].data+idx_desc);
 				const uchar* inputimg_ptr = oInputImg.data+idx_img;
 				const uchar* bgimg_ptr = m_voBGImg[nSampleIdx].data+idx_img;
 				for(int n=0;n<3; ++n) {
-					hdist[n] = hdist_ushort_8bitLUT(inputdesc[n],bgdesc_ptr[n]);
-					if(hdist[n]>nCurrSCDescDistThreshold)
+					anDescDist[n] = hdist_ushort_8bitLUT(anCurrInputDesc[n],bgdesc_ptr[n]);
+					if(anDescDist[n]>nCurrSCDescDistThreshold)
 						goto skip;
-					l1dist[n] = absdiff_uchar(inputimg_ptr[n],bgimg_ptr[n]);
-					if(l1dist[n]>nCurrSCColorDistThreshold)
+					anColorDist[n] = absdiff_uchar(inputimg_ptr[n],bgimg_ptr[n]);
+					if(anColorDist[n]>nCurrSCColorDistThreshold)
 						goto skip;
 				}
-				// @@@@@@@@@@ this is only the L1 dist, L2 might be better
-				if(hdist[0]+hdist[1]+hdist[2]<=nCurrDescDistThreshold && l1dist[0]+l1dist[1]+l1dist[2]<=nCurrColorDistThreshold)
+				if(anDescDist[0]+anDescDist[1]+anDescDist[2]<=nCurrDescDistThreshold && anColorDist[0]+anColorDist[1]+anColorDist[2]<=nCurrColorDistThreshold)
 					nGoodSamplesCount++;
 				skip:
 				nSampleIdx++;
