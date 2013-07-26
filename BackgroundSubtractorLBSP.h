d732e841d51c70497eb9fbba61c908193fa34ac4
Pierre-Luc St-Charles <pierre-luc.st-charles@polymtl.ca>
2013-07-26 15:10:45 -0400

    New default paramset for optimal CDNet results

--- BackgroundSubtractorLBSP.h
+++ BackgroundSubtractorLBSP.h
@@ -7,7 +7,7 @@
 //! defines the default value for BackgroundSubtractorLBSP::m_nDescDistThreshold
 #define BGSLBSP_DEFAULT_DESC_DIST_THRESHOLD (4)
 //! defines the default value for BackgroundSubtractorLBSP::m_nBGSamples
-#define BGSLBSP_DEFAULT_NB_BG_SAMPLES (20)
+#define BGSLBSP_DEFAULT_NB_BG_SAMPLES (35)
 //! defines the default value for BackgroundSubtractorLBSP::m_nRequiredBGSamples
 #define BGSLBSP_DEFAULT_REQUIRED_NB_BG_SAMPLES (2)
 //! defines the default value for the learning rate passed to BackgroundSubtractorLBSP::operator()
