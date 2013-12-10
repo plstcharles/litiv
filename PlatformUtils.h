dc3208e84e4ba025de2655f04cd38f621e297750
Pierre-Luc St-Charles <pierre-luc.st-charles@polymtl.ca>
2013-12-10 14:48:28 -0500

    Added memleak detection defs on W32 platform.

--- PlatformUtils.h
+++ PlatformUtils.h
@@ -4,6 +4,15 @@
 #ifdef WIN32
 #include <windows.h>
 #define PLATFORM_USES_WIN32API (WINVER>0x0599)
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#ifndef DBG_NEW
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+#define new DBG_NEW
+#endif
+#endif
 #endif //WIN32
 
 #include <queue>
