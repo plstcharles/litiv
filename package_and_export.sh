d746cb7e468bc0de45b5ba585474bd2f66abc8a9
Pierre-Luc St-Charles <pierre-luc.st-charles@polymtl.ca>
2014-01-18 12:33:13 -0500

    Added missing eclipse settings files & scripts

---
+++ package_and_export.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+	then
+	echo "No arguments supplied."
+	exit -1;
+elif [ -z "$1" ]
+	then
+	echo "Bad first arg (remote host name)."
+	exit -1;
+fi
+
+REMOTE_HOST_NAME="$1.info.polymtl.ca"
+REMOTE_PACKAGE_NAME="lbsp_pack_$1"
+REMOTE_SCRIPT_NAME="lbsp_exec_$1.sh"
+REMOTE_LOG_NAME="lbsp_exec_$1.out"
+
+echo "Creating local package..."
+mkdir -p /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME
+cp /home/perf5/workspace/lbsp/Release/lbsp /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME/
+find /usr/local/lib -name "libopencv_*" -exec cp {} /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME/ \;
+old_wd=$(pwd)
+cd /home/perf5/workspace/lbsp/Release
+echo "Compressing..."
+tar czf /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME.tar.gz $REMOTE_PACKAGE_NAME
+cd $old_wd
+
+echo "Sending to $REMOTE_HOST_NAME..."
+scp /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME.tar.gz pistc@$REMOTE_HOST_NAME:
+if [ $? -ne 0 ]
+	then
+	echo "SSH connection failed."
+	rm -r /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME
+	rm -r /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME.tar.gz
+	exit -1
+fi
+
+echo "Writing local extraction/execution script..."
+echo "#!/bin/bash
+chmod 600 $REMOTE_PACKAGE_NAME.tar.gz
+tar -xzf $REMOTE_PACKAGE_NAME.tar.gz
+chmod 700 $REMOTE_PACKAGE_NAME/lbsp
+rm $REMOTE_PACKAGE_NAME.tar.gz
+./$REMOTE_PACKAGE_NAME/lbsp"  > /home/perf5/workspace/lbsp/$REMOTE_SCRIPT_NAME
+
+echo "Sending extraction/execution script to $REMOTE_HOST_NAME..."
+scp /home/perf5/workspace/lbsp/$REMOTE_SCRIPT_NAME pistc@$REMOTE_HOST_NAME:
+if [ $? -ne 0 ]
+	then
+	echo "SSH connection failed."
+	echo "Removing local files..."
+	rm /home/perf5/workspace/lbsp/$REMOTE_SCRIPT_NAME
+	rm -r /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME
+	rm -r /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME.tar.gz
+	exit -1
+fi
+
+echo "Starting download/extract script on $REMOTE_HOST_NAME..."
+REMOTE_CMD="chmod 755 $REMOTE_SCRIPT_NAME; nohup ./$REMOTE_SCRIPT_NAME \`</dev/null\` >~/$REMOTE_LOG_NAME 2>&1 &"
+ssh pistc@$REMOTE_HOST_NAME $REMOTE_CMD
+
+echo "Removing local files..."
+rm /home/perf5/workspace/lbsp/$REMOTE_SCRIPT_NAME
+rm -r /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME
+rm -r /home/perf5/workspace/lbsp/Release/$REMOTE_PACKAGE_NAME.tar.gz
---
+++ remote_download_and_extract_cdnet.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+	then
+	echo "No arguments supplied."
+	exit -1;
+elif [ -z "$1" ]
+	then
+	echo "Bad first arg (remote host name)."
+	exit -1;
+fi
+
+REMOTE_HOST_NAME="$1.info.polymtl.ca"
+REMOTE_SCRIPT_NAME="download_and_extract_cdnet_$1.sh"
+REMOTE_LOG_NAME="download_and_extract_cdnet_$1.out"
+
+echo "Writing local download/extract script..."
+echo "#!/bin/bash
+wget -P /tmp/ http://wordpress-jodoin.dmi.usherb.ca/static/dataset/dataset.7z
+chmod 644 /tmp/dataset.7z
+7z x /tmp/dataset.7z -o/tmp/
+mkdir -p /tmp/datasets/CDNet/
+mv /tmp/dataset /tmp/datasets/CDNet/" > /home/perf5/workspace/lbsp/$REMOTE_SCRIPT_NAME
+
+echo "Sending download/extract script to $REMOTE_HOST_NAME..."
+scp /home/perf5/workspace/lbsp/$REMOTE_SCRIPT_NAME pistc@$REMOTE_HOST_NAME:
+if [ $? -ne 0 ]
+	then
+	echo "SSH connection failed."
+	echo "Removing local files..."
+	rm /home/perf5/workspace/lbsp/$REMOTE_SCRIPT_NAME
+	exit -1
+fi
+
+echo "Starting download/extract script on $REMOTE_HOST_NAME..."
+REMOTE_CMD="chmod 755 $REMOTE_SCRIPT_NAME; nohup ./$REMOTE_SCRIPT_NAME \`</dev/null\` >~/$REMOTE_LOG_NAME 2>&1 &"
+ssh pistc@$REMOTE_HOST_NAME $REMOTE_CMD
+
+echo "Removing local files..."
+rm /home/perf5/workspace/lbsp/$REMOTE_SCRIPT_NAME
+
