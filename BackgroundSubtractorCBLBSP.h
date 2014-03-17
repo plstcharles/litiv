0ce476538b7137e9d8136293c63c114ee81377ed
Pierre-Luc St-Charles <pierre-luc.st-charles@polymtl.ca>
2014-03-17 12:40:35 -0400

    Major CBLBSP cleanup/refactoring (renamed PAwCS)

--- BackgroundSubtractorCBLBSP.h
+++ BackgroundSubtractorCBLBSP.h
@@ -2,7 +2,7 @@
 
 #include "BackgroundSubtractorLBSP.h"
 
-
+ // POSSIBLE IMPROVEMENTS:
  // @@@@@ RETEST BURST FOR HIGHVAR / GHOSTS...
  // @@@@@ TEST HIGHVAR WiTH UNSTABLE AS REQUIREMENT
  // @@@@@ ADD MINIMAL R(x) THRESHOLD FOR GLOBAL DICT?
@@ -12,35 +12,24 @@
 //! defines the default offset LBSP threshold value
 #define BGSCBLBSP_DEFAULT_LBSP_OFFSET_SIMILARITY_THRESHOLD (10)
 //! defines the default value for BackgroundSubtractorLBSP::m_nDescDistThreshold
-#define BGSCBLBSP_DEFAULT_DESC_DIST_THRESHOLD (1)
+#define BGSCBLBSP_DEFAULT_MIN_DESC_DIST_THRESHOLD (1)
 //! defines the default value for BackgroundSubtractorCBLBSP::m_nColorDistThreshold
-#define BGSCBLBSP_DEFAULT_COLOR_DIST_THRESHOLD (25)
+#define BGSCBLBSP_DEFAULT_MIN_COLOR_DIST_THRESHOLD (25)
 //! defines the default value for BackgroundSubtractorCBLBSP::m_fLocalWordsPerChannel
-#define BGSCBLBSP_DEFAULT_NB_LOCAL_WORDS_PER_CH (7.0f)
+#define BGSCBLBSP_DEFAULT_MAX_NB_LOCAL_WORDS (24)
 //! defines the default value for BackgroundSubtractorCBLBSP::m_fGlobalWordsPerPixelChannel
-#define BGSCBLBSP_DEFAULT_NB_GLOBAL_WORDS_PER_CH (10.0f)
+#define BGSCBLBSP_DEFAULT_MAX_NB_GLOBAL_WORDS (30)
 //! defines the number of samples to use when computing running averages
-#define BGSCBLBSP_N_SAMPLES_FOR_MEAN (100)
-//! defines the threshold values used to detect long-term ghosting
-#define BGSCBLBSP_GHOST_DETECTION_SAVG_MIN1 (9.995f)
-#define BGSCBLBSP_GHOST_DETECTION_DLST_MAX1 (-0.005f)
-#define BGSCBLBSP_GHOST_DETECTION_SAVG_MIN2 (9.99999f)
+#define BGSCBLBSP_N_SAMPLES_FOR_ST_MVAVGS (25)
+#define BGSCBLBSP_N_SAMPLES_FOR_LT_MVAVGS (100)
 //! defines the threshold values used to detect unstable regions and edges
 #define BGSCBLBSP_INSTBLTY_DETECTION_SEGM_DIFF (0.200f)
 #define BGSCBLBSP_INSTBLTY_DETECTION_MIN_R_VAL (3.000f)
-//! defines the internal threshold adjustment factor to use when treating single channel images
-#define BGSCBLBSP_SINGLECHANNEL_THRESHOLD_MODULATION_FACT (0.400f)
 //! parameters used for dynamic distance threshold adjustments ('R(x)')
-#define BGSCBLBSP_R_INCR  (0.0100f)
-#define BGSCBLBSP_R_DECR  (0.0100f)
-#define BGSCBLBSP_R_LOWER (1.0000f)
-#define BGSCBLBSP_R_UPPER (999.0000f)
+#define BGSCBLBSP_R_VAR (0.01f)
 //! parameters used for adjusting the variation speed of dynamic distance thresholds  ('R2(x)')
-#define BGSCBLBSP_R2_OFFST (0.100f)
 #define BGSCBLBSP_R2_INCR  (1.000f)
 #define BGSCBLBSP_R2_DECR  (0.100f)
-#define BGSCBLBSP_R2_LOWER (0.100f)
-#define BGSCBLBSP_R2_UPPER (1000.f)
 //! parameters used for dynamic learning rates adjustments  ('T(x)')
 #define BGSCBLBSP_T_DECR  (0.2500f)
 #define BGSCBLBSP_T_INCR  (0.5000f)
@@ -60,12 +49,12 @@
 class BackgroundSubtractorCBLBSP : public BackgroundSubtractorLBSP {
 public:
 	//! full constructor
-	BackgroundSubtractorCBLBSP(	float fLBSPThreshold=BGSCBLBSP_DEFAULT_LBSP_REL_SIMILARITY_THRESHOLD,
+	BackgroundSubtractorCBLBSP(	float fLBSPRelThreshold=BGSCBLBSP_DEFAULT_LBSP_REL_SIMILARITY_THRESHOLD,
 								size_t nLBSPThresholdOffset=BGSCBLBSP_DEFAULT_LBSP_OFFSET_SIMILARITY_THRESHOLD,
-								size_t nInitDescDistThreshold=BGSCBLBSP_DEFAULT_DESC_DIST_THRESHOLD,
-								size_t nInitColorDistThreshold=BGSCBLBSP_DEFAULT_COLOR_DIST_THRESHOLD,
-								float fLocalWordsPerChannel=BGSCBLBSP_DEFAULT_NB_LOCAL_WORDS_PER_CH,
-								float fGlobalWordsPerChannel=BGSCBLBSP_DEFAULT_NB_GLOBAL_WORDS_PER_CH);
+								size_t nMinDescDistThreshold=BGSCBLBSP_DEFAULT_MIN_DESC_DIST_THRESHOLD,
+								size_t nMinColorDistThreshold=BGSCBLBSP_DEFAULT_MIN_COLOR_DIST_THRESHOLD,
+								size_t nMaxLocalWords=BGSCBLBSP_DEFAULT_MAX_NB_LOCAL_WORDS,
+								size_t nMaxGlobalWords=BGSCBLBSP_DEFAULT_MAX_NB_GLOBAL_WORDS);
 	//! default destructor
 	virtual ~BackgroundSubtractorCBLBSP();
 	//! (re)initiaization method; needs to be called before starting background subtraction (note: also reinitializes the keypoints vector)
@@ -113,18 +102,14 @@ protected:
 	};
 	//! indicates whether internal structures have already been initialized (LBSP lookup tables, word lists, etc.)
 	bool m_bInitializedInternalStructs;
-	//! absolute color distance threshold ('R' or 'radius' in the original ViBe paper, used as the default/initial 'R(x)' value here, paired with BackgroundSubtractorLBSP::m_nDescDistThreshold)
+	//! absolute minimal color distance threshold ('R' or 'radius' in the original ViBe paper, used as the default/initial 'R(x)' value here, paired with BackgroundSubtractorLBSP::m_nDescDistThreshold)
 	const size_t m_nColorDistThreshold;
-	//! number of local words per channel used to build a background submodel (for a single pixel)
-	const float m_fLocalWordsPerChannel;
-	//! number of local words used to build a background submodel (for a single pixel, similar to 'N' in ViBe/PBAS -- only usable after initialisation)
-	size_t m_nLocalWords;
-	//! number of global words per pixel, per channel used to build a complete background model
-	const float m_fGlobalWordsPerChannel;
-	//! number of global words used to build a complete background model (only usable after initialisation)
-	size_t m_nGlobalWords;
+	//! max/current number of local words used to build background submodels (for a single pixel, similar to 'N' in ViBe/PBAS -- may vary based on the channel size)
+	size_t m_nMaxLocalWords,m_nLocalWords;
+	//! max/current number of global words used to build the global background model (may vary based on the channel size)
+	size_t m_nMaxGlobalWords,m_nGlobalWords;
 	//! total maximum number of local dictionaries (depends on the input frame size)
-	size_t m_nMaxLocalDictionaries;
+	size_t m_nTotLocalDictionaries;
 	//! current frame index, used to keep track of word occurrence information
 	size_t m_nFrameIndex;
 	//! current number of bad continuous frames detected (used to reset the model when it gets high)
@@ -132,64 +117,54 @@ protected:
 	//! current update rate scale factor for sequences with very large ROIs
 	size_t m_nUpdateRateScaleFactor;
 
-	//! background model local word list & dictionaries
+	//! word lists, dictionaries & LUTs
 	LocalWord** m_aapLocalDicts;
 	LocalWord_1ch* m_apLocalWordList_1ch, *m_apLocalWordListIter_1ch;
 	LocalWord_3ch* m_apLocalWordList_3ch, *m_apLocalWordListIter_3ch;
-	//! background model global word list & dictionary (1x dictionary for the whole model)
 	GlobalWord** m_apGlobalDict;
 	GlobalWord_1ch* m_apGlobalWordList_1ch, *m_apGlobalWordListIter_1ch;
 	GlobalWord_3ch* m_apGlobalWordList_3ch, *m_apGlobalWordListIter_3ch;
-	GlobalWord** m_apGlobalWordLookupTable_BG;
-	GlobalWord** m_apGlobalWordLookupTable_FG;
+	GlobalWord** m_apGlobalWordLookupTable_BG, **m_apGlobalWordLookupTable_FG;
 
+	//! per-pixel update rates ('T(x)')
+	cv::Mat m_oUpdateRateFrame;
 	//! per-pixel distance thresholds ('R(x)')
 	cv::Mat m_oDistThresholdFrame;
 	//! per-pixel distance threshold variation modulators ('R2(x)')
 	cv::Mat m_oDistThresholdVariationFrame;
-	//! per-pixel mean minimal model distances ('D_min(x)')
-	cv::Mat m_oMeanMinDistFrame;
-	//! per-pixel mean minimal model distances (burst version -- 'D_min_burst(x)')
-	cv::Mat m_oMeanMinDistFrame_burst;
-	//! per-pixel mean distances between consecutive frames ('D_last(x)')
-	cv::Mat m_oMeanLastDistFrame;
-	//! per-pixel mean distances between consecutive frames (burst version -- 'D_last_burst(x)')
-	cv::Mat m_oMeanLastDistFrame_burst;
-	//! per-pixel mean raw segmentation results ('S_avg(x)')
+	//! per-pixel mean minimal model distances ('D_min^LT(x)', long-term version)
+	cv::Mat m_oMeanMinDistFrame_LT;
+	//! per-pixel mean minimal model distances ('D_min^ST(x)', short-term version)
+	cv::Mat m_oMeanMinDistFrame_ST;
+
+	//! per-pixel mean raw segmentation results
 	cv::Mat m_oMeanRawSegmResFrame;
-	//! per-pixel mean raw segmentation results (burst version -- 'S_avg_burst(x)')
-	cv::Mat m_oMeanRawSegmResFrame_burst;
-	//! per-pixel mean final segmentation results ('Z_avg(x)')
+	//! per-pixel mean final segmentation results
 	cv::Mat m_oMeanFinalSegmResFrame;
-	//! per-pixel blink detection results (@@@@@ unnamed)
-	cv::Mat m_oBlinksFrame;
-	//! per-pixel update rates ('T(x)')
-	cv::Mat m_oUpdateRateFrame;
-	//! per-pixel word weight thresholds (@@@@@ unnamed)
-	//cv::Mat m_oWeightThresholdFrame;
-	//! copy of previously used pixel intensities (used to calculate 'D_last(x)')
-	cv::Mat m_oLastColorFrame;
-	//! copy of previously used descriptors (used to calculate 'D_last(x)')
-	cv::Mat m_oLastDescFrame;
-	//! the raw foreground mask generated by the method at [t-1]
-	cv::Mat m_oPureFGMask_last;
-	//! the final foreground masks generated by the method at [t-1 & t-2]
-	cv::Mat m_oFGMask_last, m_oFGMask_last2;
-	//! the dilated final foreground mask generated by the method at [t-1]
-	cv::Mat m_oFGMask_last_dilated;
-	//! a lookup map used to keep track of ghost regions
-	cv::Mat m_oGhostRegionMask;
 	//! a lookup map used to keep track of unstable regions
 	cv::Mat m_oUnstableRegionMask;
-	//! a lookup-map used to keep track of illumination updates
+	//! a lookup map used to keep track of ghost regions (currently unused)
+	cv::Mat m_oGhostRegionMask;
+	//! a lookup map used to keep track of regions where illumination recently changed
 	cv::Mat m_oIllumUpdtRegionMask;
+	//! per-pixel blink detection results
+	cv::Mat m_oBlinksFrame;
+	//! copy of previously used pixel intensities
+	cv::Mat m_oLastColorFrame;
+	//! copy of previously used descriptors
+	cv::Mat m_oLastDescFrame;
+	//! the raw foreground mask generated by the method at [t-1]
+	cv::Mat m_oRawFGMask_last;
+	//! the final foreground masks generated by the method at [t-1]
+	cv::Mat m_oFGMask_last;
 
 	//! pre-allocated matrices used to speed up morph ops and keep by-products
 	cv::Mat m_oFGMask_PreFlood;
 	cv::Mat m_oFGMask_FloodedHoles;
+	cv::Mat m_oFGMask_last_dilated;
 	cv::Mat m_oFGMask_last_dilated_inverted;
-	cv::Mat m_oPureFGBlinkMask_curr;
-	cv::Mat m_oPureFGBlinkMask_last;
+	cv::Mat m_oRawFGBlinkMask_curr;
+	cv::Mat m_oRawFGBlinkMask_last;
 	cv::Mat m_oTempGlobalWordWeightDiffFactor;
 
 	//! pre-allocated internal LBSP threshold values for all possible 8-bit intensity values
