829e7f15bcbdf9424b230baade560b93a1ffe473
Pierre-Luc St-Charles <pierre-luc.st-charles@polymtl.ca>
2016-04-25 17:40:23 -0400

    Cleanup for gurobi/cplex/hdf5 findpackage scripts

--- cmake/Modules/FindCPLEX.cmake
+++ cmake/Modules/FindCPLEX.cmake
@@ -27,141 +27,231 @@
 #  CPLEX_LIBRARIES          - library files
 
 if(WIN32)
-  execute_process(COMMAND cmd /C set CPLEX_STUDIO_DIR OUTPUT_VARIABLE CPLEX_STUDIO_DIR_VAR ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-  if(NOT CPLEX_STUDIO_DIR_VAR)
-    MESSAGE(FATAL_ERROR "Unable to find CPLEX: environment variable CPLEX_STUDIO_DIR<VERSION> not set.")
-  endif()
-
-  STRING(REGEX REPLACE "^CPLEX_STUDIO_DIR" "" CPLEX_STUDIO_DIR_VAR ${CPLEX_STUDIO_DIR_VAR})
-  STRING(REGEX MATCH "^[0-9]+" CPLEX_WIN_VERSION ${CPLEX_STUDIO_DIR_VAR})
-  STRING(REGEX REPLACE "^[0-9]+=" "" CPLEX_STUDIO_DIR_VAR ${CPLEX_STUDIO_DIR_VAR})
-  file(TO_CMAKE_PATH "${CPLEX_STUDIO_DIR_VAR}" CPLEX_ROOT_DIR_GUESS)
-
-  set(CPLEX_WIN_VERSION ${CPLEX_WIN_VERSION} CACHE STRING "CPLEX version to be used.")
-  set(CPLEX_ROOT_DIR "${CPLEX_ROOT_DIR_GUESS}" CACHE PATH "CPLEX root directory.")
-
-  MESSAGE(STATUS "Found CLPEX version ${CPLEX_WIN_VERSION} at '${CPLEX_ROOT_DIR}'")
-
-  STRING(REGEX REPLACE "/VC/bin/.*" "" VISUAL_STUDIO_PATH ${CMAKE_C_COMPILER})
-  STRING(REGEX MATCH "Studio [0-9]+" CPLEX_WIN_VS_VERSION ${VISUAL_STUDIO_PATH})
-  STRING(REGEX REPLACE "Studio " "" CPLEX_WIN_VS_VERSION ${CPLEX_WIN_VS_VERSION})
-
-  if(${CPLEX_WIN_VS_VERSION} STREQUAL "9")
-    set(CPLEX_WIN_VS_VERSION 2008)
-  elseif(${CPLEX_WIN_VS_VERSION} STREQUAL "10")
-    set(CPLEX_WIN_VS_VERSION 2010)
-  elseif(${CPLEX_WIN_VS_VERSION} STREQUAL "11")
-    set(CPLEX_WIN_VS_VERSION 2012)
-  else()
-    MESSAGE(FATAL_ERROR "CPLEX: unknown Visual Studio version at '${VISUAL_STUDIO_PATH}'.")
-  endif()
-
-  set(CPLEX_WIN_VS_VERSION ${CPLEX_WIN_VS_VERSION} CACHE STRING "Visual Studio Version")
-
-  if("${CMAKE_C_COMPILER}" MATCHES "amd64")
-    set(CPLEX_WIN_BITNESS x64)
-  else()
-    set(CPLEX_WIN_BITNESS x86)
-  endif()
-
-  set(CPLEX_WIN_BITNESS ${CPLEX_WIN_BITNESS} CACHE STRING "On Windows: x86 or x64 (32bit resp. 64bit)")
-
-  MESSAGE(STATUS "CPLEX: using Visual Studio ${CPLEX_WIN_VS_VERSION} ${CPLEX_WIN_BITNESS} at '${VISUAL_STUDIO_PATH}'")
-
-  if(NOT CPLEX_WIN_LINKAGE)
-    set(CPLEX_WIN_LINKAGE mda CACHE STRING "CPLEX linkage variant on Windows. One of these: mda (dll, release), mdd (dll, debug), mta (static, release), mtd (static, debug)")
-  endif(NOT CPLEX_WIN_LINKAGE)
-
-  # now, generate platform string
-  set(CPLEX_WIN_PLATFORM "${CPLEX_WIN_BITNESS}_windows_vs${CPLEX_WIN_VS_VERSION}/stat_${CPLEX_WIN_LINKAGE}")
-
+    execute_process(COMMAND cmd /C set CPLEX_STUDIO_DIR OUTPUT_VARIABLE CPLEX_STUDIO_DIR_VAR ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT CPLEX_STUDIO_DIR_VAR)
+        MESSAGE(FATAL_ERROR "Unable to find CPLEX: environment variable CPLEX_STUDIO_DIR<VERSION> not set.")
+    endif()
+    STRING(REGEX REPLACE "^CPLEX_STUDIO_DIR" "" CPLEX_STUDIO_DIR_VAR ${CPLEX_STUDIO_DIR_VAR})
+    STRING(REGEX MATCH "^[0-9]+" CPLEX_WIN_VERSION ${CPLEX_STUDIO_DIR_VAR})
+    STRING(REGEX REPLACE "^[0-9]+=" "" CPLEX_STUDIO_DIR_VAR ${CPLEX_STUDIO_DIR_VAR})
+    file(TO_CMAKE_PATH "${CPLEX_STUDIO_DIR_VAR}" CPLEX_ROOT_DIR_GUESS)
+    set(CPLEX_WIN_VERSION ${CPLEX_WIN_VERSION} CACHE STRING "CPLEX version to be used.")
+    set(CPLEX_ROOT_DIR "${CPLEX_ROOT_DIR_GUESS}" CACHE PATH "CPLEX root directory.")
+    MESSAGE(STATUS "Found CLPEX version ${CPLEX_WIN_VERSION} at '${CPLEX_ROOT_DIR}'")
+    STRING(REGEX REPLACE "/VC/bin/.*" "" VISUAL_STUDIO_PATH ${CMAKE_C_COMPILER})
+    STRING(REGEX MATCH "Studio [0-9]+" CPLEX_WIN_VS_VERSION ${VISUAL_STUDIO_PATH})
+    STRING(REGEX REPLACE "Studio " "" CPLEX_WIN_VS_VERSION ${CPLEX_WIN_VS_VERSION})
+    if(CPLEX_WIN_VS_VERSION STREQUAL "10")
+        set(CPLEX_WIN_VS_VERSION "2010")
+    elseif(CPLEX_WIN_VS_VERSION STREQUAL "11")
+        set(CPLEX_WIN_VS_VERSION "2012")
+    elseif(CPLEX_WIN_VS_VERSION STREQUAL "12")
+        set(CPLEX_WIN_VS_VERSION "2013")
+    elseif(CPLEX_WIN_VS_VERSION STREQUAL "14")
+        set(CPLEX_WIN_VS_VERSION "2015")
+    else()
+        message(FATAL_ERROR "Unsupported MSVC compiler version: ${CPLEX_WIN_VS_VERSION}")
+    endif()
+    set(CPLEX_WIN_VS_VERSION ${CPLEX_WIN_VS_VERSION} CACHE STRING "Visual Studio Version")
+    if("${CMAKE_C_COMPILER}" MATCHES "amd64")
+        set(CPLEX_WIN_BITNESS x64)
+    else()
+        set(CPLEX_WIN_BITNESS x86)
+    endif()
+    set(CPLEX_WIN_BITNESS ${CPLEX_WIN_BITNESS} CACHE STRING "On Windows: x86 or x64 (32bit resp. 64bit)")
+    MESSAGE(STATUS "CPLEX: using Visual Studio ${CPLEX_WIN_VS_VERSION} ${CPLEX_WIN_BITNESS} at '${VISUAL_STUDIO_PATH}'")
+    if(NOT CPLEX_WIN_LINKAGE)
+        set(CPLEX_WIN_LINKAGE mda CACHE STRING "CPLEX linkage variant on Windows. One of these: mda (dll, release), mdd (dll, debug), mta (static, release), mtd (static, debug)")
+    endif(NOT CPLEX_WIN_LINKAGE)
+    set(CPLEX_WIN_PLATFORM "${CPLEX_WIN_BITNESS}_windows_vs${CPLEX_WIN_VS_VERSION}/stat_${CPLEX_WIN_LINKAGE}")
 else()
-
-  set(CPLEX_ROOT_DIR "" CACHE PATH "CPLEX root directory.")
-  set(CPLEX_WIN_PLATFORM "")
-
+    set(CPLEX_ROOT_DIR "" CACHE PATH "CPLEX root directory.")
+    set(CPLEX_WIN_PLATFORM "")
 endif()
 
-
 FIND_PATH(CPLEX_INCLUDE_DIR
-  ilcplex/cplex.h
-  HINTS ${CPLEX_ROOT_DIR}/cplex/include
+    NAMES
+        ilcplex/cplex.h
+    HINTS
+        ${CPLEX_ROOT_DIR}/cplex/include
         ${CPLEX_ROOT_DIR}/include
-  PATHS ENV C_INCLUDE_PATH
+        $ENV{CPLEX_ROOT}/cplex/include
+        $ENV{CPLEX_ROOT}/include
+    PATHS
+        ENV C_INCLUDE_PATH
         ENV C_PLUS_INCLUDE_PATH
         ENV INCLUDE_PATH
-  )
+)
 
 FIND_PATH(CPLEX_CONCERT_INCLUDE_DIR
-  ilconcert/iloenv.h
-  HINTS ${CPLEX_ROOT_DIR}/concert/include
+    NAMES
+        ilconcert/iloenv.h
+    HINTS
+        ${CPLEX_ROOT_DIR}/concert/include
         ${CPLEX_ROOT_DIR}/include
-  PATHS ENV C_INCLUDE_PATH
+        $ENV{CPLEX_ROOT}/concert/include
+        $ENV{CPLEX_ROOT}/include
+        $ENV{CPLEX_ROOT}/../concert/include
+    PATHS
+        ENV C_INCLUDE_PATH
         ENV C_PLUS_INCLUDE_PATH
         ENV INCLUDE_PATH
-  )
+)
 
 FIND_LIBRARY(CPLEX_LIBRARY
-  NAMES cplex${CPLEX_WIN_VERSION} cplex
-  HINTS ${CPLEX_ROOT_DIR}/cplex/lib/${CPLEX_WIN_PLATFORM} #windows
-        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_debian4.0_4.1/static_pic #unix
-        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_sles10_4.1/static_pic #unix
-        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic #osx
-  PATHS ENV LIBRARY_PATH #unix
-        ENV LD_LIBRARY_PATH #unix
-  )
+    NAMES
+        cplex
+        cplex${CPLEX_WIN_VERSION}
+    HINTS
+        ${CPLEX_ROOT_DIR}/cplex/lib/${CPLEX_WIN_PLATFORM}
+        ${CPLEX_ROOT_DIR}/lib/${CPLEX_WIN_PLATFORM}
+        $ENV{CPLEX_ROOT}/cplex/lib/${CPLEX_WIN_PLATFORM}
+        $ENV{CPLEX_ROOT}/lib/${CPLEX_WIN_PLATFORM}
+        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_debian4.0_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_debian4.0_4.1/static_pic
+        $ENV{CPLEX_ROOT}/cplex/lib/x86-64_debian4.0_4.1/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_debian4.0_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_sles10_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_sles10_4.1/static_pic
+        $ENV{CPLEX_ROOT}/cplex/lib/x86-64_sles10_4.1/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_sles10_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_linux/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_linux/static_pic
+        $ENV{CPLEX_ROOT}/cplex/lib/x86-64_linux/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_linux/static_pic
+        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_osx/static_pic
+        $ENV{CPLEX_ROOT}/cplex/lib/x86-64_osx/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_osx/static_pic
+    PATHS
+        ENV LIBRARY_PATH
+        ENV LD_LIBRARY_PATH
+)
 message(STATUS "CPLEX Library: ${CPLEX_LIBRARY}")
 
 FIND_LIBRARY(CPLEX_ILOCPLEX_LIBRARY
-  ilocplex
-  HINTS ${CPLEX_ROOT_DIR}/cplex/lib/${CPLEX_WIN_PLATFORM} #windows
-        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_debian4.0_4.1/static_pic #unix
-        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_sles10_4.1/static_pic #unix
-        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic #osx
-  PATHS ENV LIBRARY_PATH
+    NAMES
+        ilocplex
+    HINTS
+        ${CPLEX_ROOT_DIR}/cplex/lib/${CPLEX_WIN_PLATFORM}
+        ${CPLEX_ROOT_DIR}/lib/${CPLEX_WIN_PLATFORM}
+        $ENV{CPLEX_ROOT}/cplex/lib/${CPLEX_WIN_PLATFORM}
+        $ENV{CPLEX_ROOT}/lib/${CPLEX_WIN_PLATFORM}
+        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_debian4.0_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_debian4.0_4.1/static_pic
+        $ENV{CPLEX_ROOT}/cplex/lib/x86-64_debian4.0_4.1/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_debian4.0_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_sles10_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_sles10_4.1/static_pic
+        $ENV{CPLEX_ROOT}/cplex/lib/x86-64_sles10_4.1/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_sles10_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_linux/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_linux/static_pic
+        $ENV{CPLEX_ROOT}/cplex/lib/x86-64_linux/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_linux/static_pic
+        ${CPLEX_ROOT_DIR}/cplex/lib/x86-64_osx/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_osx/static_pic
+        $ENV{CPLEX_ROOT}/cplex/lib/x86-64_osx/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_osx/static_pic
+    PATHS
+        ENV LIBRARY_PATH
         ENV LD_LIBRARY_PATH
-  )
+)
 message(STATUS "ILOCPLEX Library: ${CPLEX_ILOCPLEX_LIBRARY}")
 
 FIND_LIBRARY(CPLEX_CONCERT_LIBRARY
-  concert
-  HINTS ${CPLEX_ROOT_DIR}/concert/lib/${CPLEX_WIN_PLATFORM} #windows
-        ${CPLEX_ROOT_DIR}/concert/lib/x86-64_debian4.0_4.1/static_pic #unix
-        ${CPLEX_ROOT_DIR}/concert/lib/x86-64_sles10_4.1/static_pic #unix
-        ${CPLEX_ROOT_DIR}/concert/lib/x86-64_osx/static_pic #osx
-  PATHS ENV LIBRARY_PATH
+    NAMES
+        concert
+    HINTS
+        ${CPLEX_ROOT_DIR}/concert/lib/${CPLEX_WIN_PLATFORM}
+        ${CPLEX_ROOT_DIR}/../concert/lib/${CPLEX_WIN_PLATFORM}
+        ${CPLEX_ROOT_DIR}/lib/${CPLEX_WIN_PLATFORM}
+        $ENV{CPLEX_ROOT}/concert/lib/${CPLEX_WIN_PLATFORM}
+        $ENV{CPLEX_ROOT}/../concert/lib/${CPLEX_WIN_PLATFORM}
+        $ENV{CPLEX_ROOT}/lib/${CPLEX_WIN_PLATFORM}
+        ${CPLEX_ROOT_DIR}/concert/lib/x86-64_debian4.0_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/../concert/lib/x86-64_debian4.0_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_debian4.0_4.1/static_pic
+        $ENV{CPLEX_ROOT}/concert/lib/x86-64_debian4.0_4.1/static_pic
+        $ENV{CPLEX_ROOT}/../concert/lib/x86-64_debian4.0_4.1/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_debian4.0_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/concert/lib/x86-64_sles10_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/../concert/lib/x86-64_sles10_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_sles10_4.1/static_pic
+        $ENV{CPLEX_ROOT}/concert/lib/x86-64_sles10_4.1/static_pic
+        $ENV{CPLEX_ROOT}/../concert/lib/x86-64_sles10_4.1/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_sles10_4.1/static_pic
+        ${CPLEX_ROOT_DIR}/concert/lib/x86-64_linux/static_pic
+        ${CPLEX_ROOT_DIR}/../concert/lib/x86-64_linux/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_linux/static_pic
+        $ENV{CPLEX_ROOT}/concert/lib/x86-64_linux/static_pic
+        $ENV{CPLEX_ROOT}/../concert/lib/x86-64_linux/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_linux/static_pic
+        ${CPLEX_ROOT_DIR}/concert/lib/x86-64_osx/static_pic
+        ${CPLEX_ROOT_DIR}/../concert/lib/x86-64_osx/static_pic
+        ${CPLEX_ROOT_DIR}/lib/x86-64_osx/static_pic
+        $ENV{CPLEX_ROOT}/concert/lib/x86-64_osx/static_pic
+        $ENV{CPLEX_ROOT}/../concert/lib/x86-64_osx/static_pic
+        $ENV{CPLEX_ROOT}/lib/x86-64_osx/static_pic
+    PATHS
+        ENV LIBRARY_PATH
         ENV LD_LIBRARY_PATH
-  )
+)
 message(STATUS "CONCERT Library: ${CPLEX_CONCERT_LIBRARY}")
 
 if(WIN32)
 	FIND_PATH(CPLEX_BIN_DIR
-	  cplex${CPLEX_WIN_VERSION}.dll
-          HINTS ${CPLEX_ROOT_DIR}/cplex/bin/${CPLEX_WIN_PLATFORM} #windows
-	  )
+        NAMES
+            cplex${CPLEX_WIN_VERSION}.dll
+        HINTS
+            ${CPLEX_ROOT_DIR}/cplex/bin/${CPLEX_WIN_PLATFORM}
+            ${CPLEX_ROOT_DIR}/bin/${CPLEX_WIN_PLATFORM}
+            $ENV{CPLEX_ROOT}/cplex/bin/${CPLEX_WIN_PLATFORM}
+            $ENV{CPLEX_ROOT}/bin/${CPLEX_WIN_PLATFORM}
+    )
 else()
 	FIND_PATH(CPLEX_BIN_DIR
-	  cplex
-          HINTS ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_sles10_4.1 #unix
-                ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_debian4.0_4.1 #unix
-                ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_osx #osx
-	  ENV LIBRARY_PATH
-          ENV LD_LIBRARY_PATH
-	  )
+        NAMES
+	        cplex
+        HINTS
+            ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_sles10_4.1
+            ${CPLEX_ROOT_DIR}/bin/x86-64_sles10_4.1
+            ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_debian4.0_4.1
+            ${CPLEX_ROOT_DIR}/bin/x86-64_debian4.0_4.1
+            ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_linux
+            ${CPLEX_ROOT_DIR}/bin/x86-64_linux
+            ${CPLEX_ROOT_DIR}/cplex/bin/x86-64_osx
+            ${CPLEX_ROOT_DIR}/bin/x86-64_osx
+            $ENV{CPLEX_ROOT}/cplex/bin/x86-64_sles10_4.1
+            $ENV{CPLEX_ROOT}/bin/x86-64_sles10_4.1
+            $ENV{CPLEX_ROOT}/cplex/bin/x86-64_debian4.0_4.1
+            $ENV{CPLEX_ROOT}/bin/x86-64_debian4.0_4.1
+            $ENV{CPLEX_ROOT}/cplex/bin/x86-64_linux
+            $ENV{CPLEX_ROOT}/bin/x86-64_linux
+            $ENV{CPLEX_ROOT}/cplex/bin/x86-64_osx
+            $ENV{CPLEX_ROOT}/bin/x86-64_osx
+        PATHS
+            ENV LIBRARY_PATH
+            ENV LD_LIBRARY_PATH
+    )
 endif()
 message(STATUS "CPLEX Bin Dir: ${CPLEX_BIN_DIR}")
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(CPLEX DEFAULT_MSG
- CPLEX_LIBRARY CPLEX_INCLUDE_DIR CPLEX_ILOCPLEX_LIBRARY CPLEX_CONCERT_LIBRARY CPLEX_CONCERT_INCLUDE_DIR)
+    CPLEX_LIBRARY
+    CPLEX_INCLUDE_DIR
+    CPLEX_ILOCPLEX_LIBRARY
+    CPLEX_CONCERT_LIBRARY
+    CPLEX_CONCERT_INCLUDE_DIR
+)
 
 IF(CPLEX_FOUND)
-  SET(CPLEX_INCLUDE_DIRS ${CPLEX_INCLUDE_DIR} ${CPLEX_CONCERT_INCLUDE_DIR})
-  SET(CPLEX_LIBRARIES ${CPLEX_CONCERT_LIBRARY} ${CPLEX_ILOCPLEX_LIBRARY} ${CPLEX_LIBRARY} )
-  IF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    SET(CPLEX_LIBRARIES "${CPLEX_LIBRARIES};m;pthread")
-  ENDIF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    SET(CPLEX_INCLUDE_DIRS ${CPLEX_INCLUDE_DIR} ${CPLEX_CONCERT_INCLUDE_DIR})
+    SET(CPLEX_LIBRARIES ${CPLEX_CONCERT_LIBRARY} ${CPLEX_ILOCPLEX_LIBRARY} ${CPLEX_LIBRARY} )
+    IF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+        SET(CPLEX_LIBRARIES "${CPLEX_LIBRARIES};m;pthread")
+    ENDIF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 ENDIF(CPLEX_FOUND)
 
 MARK_AS_ADVANCED(CPLEX_LIBRARY CPLEX_INCLUDE_DIR CPLEX_ILOCPLEX_LIBRARY CPLEX_CONCERT_INCLUDE_DIR CPLEX_CONCERT_LIBRARY)
--- cmake/Modules/FindGUROBI.cmake
+++ cmake/Modules/FindGUROBI.cmake
@@ -26,75 +26,90 @@
 #   GUROBI_INCLUDE_DIRS - The Gurobi include directories
 #   GUROBI_LIBRARIES - The libraries needed to use Gurobi
 
-if (GUROBI_INCLUDE_DIR)
-  # in cache already
-  set(GUROBI_FOUND TRUE)
-  set(GUROBI_INCLUDE_DIRS "${GUROBI_INCLUDE_DIR}" )
-  set(GUROBI_LIBRARIES "${GUROBI_CXX_LIBRARY};${GUROBI_LIBRARY}" )
-else (GUROBI_INCLUDE_DIR)
+if(GUROBI_INCLUDE_DIR)
+    # in cache already
+    set(GUROBI_FOUND TRUE)
+    set(GUROBI_INCLUDE_DIRS "${GUROBI_INCLUDE_DIR}" )
+    set(GUROBI_LIBRARIES "${GUROBI_CXX_LIBRARY};${GUROBI_LIBRARY}" )
+else(GUROBI_INCLUDE_DIR)
 
-find_path(GUROBI_INCLUDE_DIR
-          NAMES gurobi_c++.h
-          PATHS "$ENV{GUROBI_HOME}/include"
-                  "/Library/gurobi502/mac64/include"
-                 "C:\\libs\\gurobi502\\include"
-          )
+    find_path(GUROBI_INCLUDE_DIR
+        NAMES
+            gurobi_c++.h
+        HINTS
+            "$ENV{GUROBI_HOME}/include"
+            "$ENV{GUROBI_ROOT}/include"
+            "/Library/gurobi651/mac64/include"
+            "C:\\libs\\gurobi651\\include"
+            "/opt/gurobi/include"
+            "/opt/gurobi/gurobi651/include"
+            "/opt/gurobi651/include"
+    )
 
-find_library( GUROBI_LIBRARY
-              NAMES gurobi
-gurobi45
-gurobi46
-        gurobi50
-        gurobi51
-        gurobi52
-        gurobi55
-        gurobi60
-              PATHS "$ENV{GUROBI_HOME}/lib"
-                    "/Library/gurobi502/mac64/lib"
-                    "C:\\libs\\gurobi502\\lib"
-              )
+    find_library(GUROBI_LIBRARY
+        NAMES
+            gurobi
+            gurobi45
+            gurobi46
+            gurobi50
+            gurobi51
+            gurobi52
+            gurobi55
+            gurobi60
+            gurobi65
+        PATHS
+            "$ENV{GUROBI_HOME}/lib"
+            "$ENV{GUROBI_ROOT}/lib"
+            "/Library/gurobi651/mac64/lib"
+            "C:\\libs\\gurobi651\\lib"
+            "/opt/gurobi/lib"
+            "/opt/gurobi/gurobi651/lib"
+            "/opt/gurobi651/lib"
+    )
 
-if (MSVC)
-  STRING(REGEX REPLACE "/VC/bin/.*" "" VISUAL_STUDIO_PATH ${CMAKE_C_COMPILER})
+    if(MSVC)
+        STRING(REGEX REPLACE "/VC/bin/.*" "" VISUAL_STUDIO_PATH ${CMAKE_C_COMPILER})
+        STRING(REGEX MATCH "Studio [0-9]+" VISUAL_STUDIO_VERSION ${VISUAL_STUDIO_PATH})
+        STRING(REGEX REPLACE "Studio " "" VISUAL_STUDIO_VERSION ${VISUAL_STUDIO_VERSION})
+        if(VISUAL_STUDIO_VERSION STREQUAL "10")
+            set(VISUAL_STUDIO_YEAR "2010")
+        elseif(VISUAL_STUDIO_VERSION STREQUAL "11")
+            set(VISUAL_STUDIO_YEAR "2012")
+        elseif(VISUAL_STUDIO_VERSION STREQUAL "12")
+            set(VISUAL_STUDIO_YEAR "2013")
+        elseif(VISUAL_STUDIO_VERSION STREQUAL "14")
+            set(VISUAL_STUDIO_YEAR "2015")
+        else()
+            message(FATAL_ERROR "Unsupported MSVC compiler version: ${VISUAL_STUDIO_VERSION}")
+        endif()
+        set(GUROBI_LIB_NAME gurobi_c++md${VISUAL_STUDIO_YEAR})
+    else()
+        set(GUROBI_LIB_NAME gurobi_c++)
+    endif()
 
-  STRING(REGEX MATCH "Studio [0-9]+" VISUAL_STUDIO_VERSION ${VISUAL_STUDIO_PATH})
-  STRING(REGEX REPLACE "Studio " "" VISUAL_STUDIO_VERSION ${VISUAL_STUDIO_VERSION})
+    find_library(GUROBI_CXX_LIBRARY
+        NAMES
+            ${GUROBI_LIB_NAME}
+        PATHS
+            "$ENV{GUROBI_HOME}/lib"
+            "$ENV{GUROBI_ROOT}/lib"
+            "/Library/gurobi651/mac64/lib"
+            "C:\\libs\\gurobi651\\lib"
+            "/opt/gurobi/lib"
+            "/opt/gurobi/gurobi651/lib"
+            "/opt/gurobi651/lib"
+    )
 
-  if (VISUAL_STUDIO_VERSION STREQUAL "10")
-    set (VISUAL_STUDIO_YEAR "2010")
-  elseif (VISUAL_STUDIO_VERSION STREQUAL "11")
-    set (VISUAL_STUDIO_YEAR "2012")
-  elseif (VISUAL_STUDIO_VERSION STREQUAL "12")
-    set (VISUAL_STUDIO_YEAR "2013")
-  else ()
-    message(FATAL_ERROR "Unsupported compiler version: ${VISUAL_STUDIO_VERSION}")
-  endif ()
+    set(GUROBI_INCLUDE_DIRS "${GUROBI_INCLUDE_DIR}" )
+    set(GUROBI_LIBRARIES "${GUROBI_CXX_LIBRARY};${GUROBI_LIBRARY}" )
 
-  set (GUROBI_LIB_NAME gurobi_c++md${VISUAL_STUDIO_YEAR})
-else ()
-  set (GUROBI_LIB_NAME gurobi_c++)
-endif ()
+    # use c++ headers as default
+    # set(GUROBI_COMPILER_FLAGS "-DIL_STD" CACHE STRING "Gurobi Compiler Flags")
 
-
-find_library( GUROBI_CXX_LIBRARY
-              NAMES ${GUROBI_LIB_NAME}
-              PATHS "$ENV{GUROBI_HOME}/lib"
-                    "/Library/gurobi502/mac64/lib"
-                    "C:\\libs\\gurobi502\\lib"
-              )
-
-set(GUROBI_INCLUDE_DIRS "${GUROBI_INCLUDE_DIR}" )
-set(GUROBI_LIBRARIES "${GUROBI_CXX_LIBRARY};${GUROBI_LIBRARY}" )
-
-# use c++ headers as default
-# set(GUROBI_COMPILER_FLAGS "-DIL_STD" CACHE STRING "Gurobi Compiler Flags")
-
-include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set LIBCPLEX_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(GUROBI DEFAULT_MSG
-                                  GUROBI_LIBRARY GUROBI_CXX_LIBRARY GUROBI_INCLUDE_DIR)
-
-mark_as_advanced(GUROBI_INCLUDE_DIR GUROBI_LIBRARY GUROBI_CXX_LIBRARY)
+    include(FindPackageHandleStandardArgs)
+    # handle the QUIETLY and REQUIRED arguments and set LIBGUROBI_FOUND to TRUE
+    # if all listed variables are TRUE
+    find_package_handle_standard_args(GUROBI DEFAULT_MSG GUROBI_LIBRARY GUROBI_CXX_LIBRARY GUROBI_INCLUDE_DIR)
+    mark_as_advanced(GUROBI_INCLUDE_DIR GUROBI_LIBRARY GUROBI_CXX_LIBRARY)
 
 endif(GUROBI_INCLUDE_DIR)
