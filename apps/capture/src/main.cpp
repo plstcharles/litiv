4bd2274e3dd3d51fba5511fba008868bcf0f7faf
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2016-05-31 14:09:03 -0400

    Update capture main to use ocv compat grabber

--- apps/capture/src/main.cpp
+++ apps/capture/src/main.cpp
@@ -18,213 +18,32 @@
 #include "litiv/datasets.hpp"
 #include "litiv/video.hpp"
 
-#include "litiv/utils/PlatformUtils.hpp"
 #include "litiv/3rdparty/DShowBase/streams.h"
-#include "litiv/3rdparty/DShowBase/dshowutil.h"
-#include <atlbase.h>
+#include "litiv/3rdparty/DShowBase/ocvcompat.h"
 #include <Kinect.h>
 
 ////////////////////////////////
 #define WRITE_OUTPUT            0
-#define DISPLAY_OUTPUT          1
+#define DISPLAY_OUTPUT          0
 ////////////////////////////////
-
-const UINT WM_GRAPH_EVENT = WM_APP + 1;
-typedef void (CALLBACK *GraphEventFN)(HWND hwnd, long eventCode, LONG_PTR param1, LONG_PTR param2);
-
-struct DShowCaptureManager {
-
-    DShowCaptureManager(HWND hwnd) {
-        m_hwnd = hwnd;
-        m_bIsRunning = false;
-        m_pBuilder = nullptr;
-        m_pControl = nullptr;
-        m_pCam = nullptr;
-        m_pVMR9 = nullptr;
-        m_pEvent = nullptr;
-        m_pFilterGraph = nullptr;
-        m_pCapBuilder = nullptr;
-        BuildGraph();
-    }
-
-    ~DShowCaptureManager() {
-        TearDownGraph();
-    }
-
-    HRESULT Play() {
-        if(m_bIsRunning)
-            return VFW_E_WRONG_STATE;
-        HRESULT hr = m_pControl->Run();
-        m_bIsRunning = SUCCEEDED(hr);
-        return hr;
-    }
-
-    HRESULT Stop() {
-        if(!m_bIsRunning)
-            return VFW_E_WRONG_STATE;
-        HRESULT hr = m_pControl->Stop();
-        m_bIsRunning = !SUCCEEDED(hr);
-        return hr;
-    }
-
-    HRESULT HandleGraphEvent(GraphEventFN pfnOnGraphEvent) {
-        if(!m_pEvent)
-            return E_UNEXPECTED;
-        long evCode = 0;
-        LONG_PTR param1 = 0, param2 = 0;
-        HRESULT hr = S_OK;
-        while(SUCCEEDED(m_pEvent->GetEvent(&evCode,&param1,&param2,0))) {
-            pfnOnGraphEvent(m_hwnd,evCode,param1,param2);
-            hr = m_pEvent->FreeEventParams(evCode,param1,param2);
-            if(FAILED(hr))
-                break;
-        }
-        return hr;
-    }
-
-private:
-
-    void BuildGraph() {
-        TearDownGraph();
-        lvAssertHR(CoCreateInstance(CLSID_FilterGraph,NULL,CLSCTX_INPROC_SERVER,IID_PPV_ARGS(&m_pBuilder)));
-        lvAssertHR(CoCreateInstance(CLSID_CaptureGraphBuilder2,NULL,CLSCTX_INPROC_SERVER,IID_PPV_ARGS(&m_pCapBuilder)));
-        lvAssertHR(m_pCapBuilder->SetFiltergraph(m_pBuilder));
-        lvAssertHR(m_pBuilder->QueryInterface(IID_PPV_ARGS(&m_pControl)));
-        lvAssertHR(m_pBuilder->QueryInterface(IID_PPV_ARGS(&m_pEvent)));
-        lvAssertHR(m_pEvent->SetNotifyWindow((OAHWND)m_hwnd,WM_GRAPH_EVENT,NULL));
-        lvAssertHR(m_pBuilder->QueryInterface(IID_PPV_ARGS(&m_pFilterGraph)));
-        lvAssertHR(AddFilterByName(m_pBuilder,"FLIR ThermaCAM",&m_pCam,CLSID_VideoInputDeviceCategory,L"FLIR ThermaCAM"));
-        lvAssertHR(AddFilterByCLSID(m_pBuilder,CLSID_VideoMixingRenderer9,&m_pVMR9,L"VMR9"));
-        lvAssertHR(m_pCapBuilder->RenderStream(NULL,NULL,m_pCam,NULL,m_pVMR9));
-        Play();
-    }
-
-    void TearDownGraph() {
-        Stop();
-        if(m_pEvent)
-            m_pEvent->SetNotifyWindow((OAHWND)NULL,NULL,NULL);
-        CComPtr<IEnumFilters> pEnum;
-        CComPtr<IBaseFilter> pFilter;
-        if(m_pBuilder) {
-            if(SUCCEEDED(m_pBuilder->EnumFilters(&pEnum))) {
-                while(pEnum->Next(1,&pFilter,NULL)==S_OK) {
-                    m_pBuilder->RemoveFilter(pFilter);
-                    pEnum->Reset();
-                    pFilter = nullptr;
-                }
-                pEnum = nullptr;
-            }
-        }
-        m_pVMR9 = nullptr;
-        m_pCam = nullptr;
-        m_pEvent = nullptr;
-        m_pControl = nullptr;
-        m_pCapBuilder = nullptr;
-        m_pBuilder = nullptr;
-    }
-
-    HWND m_hwnd;
-    bool m_bIsRunning;
-    CComPtr<IGraphBuilder> m_pBuilder;
-    CComPtr<IMediaControl> m_pControl;
-    CComPtr<IMediaEventEx> m_pEvent;
-    CComPtr<IBaseFilter> m_pCam;
-    CComPtr<IBaseFilter> m_pVMR9;
-    CComPtr<IFilterGraph2> m_pFilterGraph;
-    CComPtr<ICaptureGraphBuilder2> m_pCapBuilder;
-};
-
-DShowCaptureManager* g_pPlayer = nullptr;
-
-void CALLBACK OnGraphEvent(HWND hwnd, long evCode, LONG_PTR param1, LONG_PTR param2) {
-    switch (evCode) {
-        case EC_COMPLETE:
-        case EC_USERABORT:
-            g_pPlayer->Stop();
-            break;
-        case EC_ERRORABORT:
-            MessageBox(hwnd, L"Playback error.", TEXT("Error"), MB_OK | MB_ICONERROR);
-            g_pPlayer->Stop();
-            break;
-    }
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-        case WM_CREATE:
-            g_pPlayer = new (std::nothrow) DShowCaptureManager(hwnd);
-            if(g_pPlayer==NULL)
-                return -1;
-            return 0;
-        case WM_DESTROY:
-            if(g_pPlayer)
-                delete g_pPlayer;
-            PostQuitMessage(0);
-            return 0;
-        case WM_GRAPH_EVENT:
-            g_pPlayer->HandleGraphEvent(OnGraphEvent);
-            return 0;
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
-}
-
-BOOL CtrlHandler(DWORD fdwCtrlType) {
-    switch( fdwCtrlType ) {
-        case CTRL_C_EVENT:
-        case CTRL_CLOSE_EVENT:
-        case CTRL_BREAK_EVENT:
-        case CTRL_LOGOFF_EVENT:
-        case CTRL_SHUTDOWN_EVENT:
-        default:
-            if(g_pPlayer)
-                delete g_pPlayer;
-            return FALSE;
-    }
-}
 
 int main() {
     try {
-        lvAssert(SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler,true));
-        HINSTANCE hInstance = GetModuleHandle(NULL);
         lvAssertHR(CoInitializeEx(0,COINIT_MULTITHREADED|COINIT_DISABLE_OLE1DDE));
-        const wchar_t CLASS_NAME[]  = L"Sample Window Class";
-        WNDCLASS wc = { };
-        wc.lpfnWndProc   = WindowProc;
-        wc.hInstance     = hInstance;
-        wc.lpszClassName = CLASS_NAME;
-        RegisterClass(&wc);
-        HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"DirectShow Playback",
-            WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-            CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
-        if (hwnd == NULL) {
-            MessageBox(hwnd, L"CreateWindowEx failed.", TEXT("Error"), MB_OK | MB_ICONERROR);
-            return 0;
-        }
-        MSG msg = { };
-        while(GetMessage(&msg, NULL, 0, 0)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-
-        /*CComPtr<IKinectSensor> pSensor;
-        if(FAILED(GetDefaultKinectSensor(&pSensor))) {
-            std::cout << "\nCould not connect to a Kinect sensor." << std::endl;
-            return 1;
-        }
-        CV_Assert(pSensor);
-        lvAssertHR(pSensor->Open());
+        std::unique_ptr<litiv::DShowCameraGrabber> pFLIRSensor = std::make_unique<litiv::DShowCameraGrabber>("FLIR ThermaCAM",(bool)DISPLAY_OUTPUT);
+        lvAssertHR(pFLIRSensor->Connect());
+        CComPtr<IKinectSensor> pKinectSensor;
+        lvAssertHR(GetDefaultKinectSensor(&pKinectSensor));
+        lvAssert(pKinectSensor);
+        lvAssertHR(pKinectSensor->Open());
         CComPtr<IMultiSourceFrameReader> pMultiFrameReader;
-        lvAssertHR(pSensor->OpenMultiSourceFrameReader(FrameSourceTypes_Color|
-            FrameSourceTypes_Depth|
-            FrameSourceTypes_Infrared|
-            FrameSourceTypes_Body|
-            FrameSourceTypes_BodyIndex|
-            0,&pMultiFrameReader));
-        cv::Mat oNIRFrame,oDepthFrame,oColorFrame;
+        lvAssertHR(pKinectSensor->OpenMultiSourceFrameReader(FrameSourceTypes_Color|FrameSourceTypes_Depth|FrameSourceTypes_Infrared|FrameSourceTypes_Body|FrameSourceTypes_BodyIndex,&pMultiFrameReader));
+        cv::Mat oFIRFrame,oNIRFrame,oDepthFrame,oColorFrame;
         size_t nFrameIdx = 0;
         while(true) {
             CComPtr<IMultiSourceFrame> pMultiFrame;
             while(!SUCCEEDED(pMultiFrameReader->AcquireLatestFrame(&pMultiFrame)));
+            pFLIRSensor->GetLatestFrame(oFIRFrame,true);
             bool bGotNIR=false, bGotDepth=false, bGotColor=false;
             {
                 CComPtr<IInfraredFrameReference> pNIRFrameRef;
@@ -281,7 +100,7 @@ int main() {
                     lvAssertHR(pColorFrame->CopyConvertedFrameDataToArray(oColorFrame.total()*oColorFrame.elemSize(),oColorFrame.data,ColorImageFormat_Bgra));
                 }
             }
-
+            cv::imshow("oFIRFrame",oFIRFrame);
             if(bGotNIR)
                 cv::imshow("oNIRFrame",oNIRFrame);
             else
@@ -294,10 +113,9 @@ int main() {
                 cv::imshow("oColorFrame",oColorFrame);
             else
                 std::cout << "#" << nFrameIdx << " color failed" << std::endl;
-
-            cv::waitKey(0);
+            cv::waitKey(1);
             ++nFrameIdx;
-        }*/
+        }
     }
     catch(const cv::Exception& e) {std::cout << "\n!!!!!!!!!!!!!!\nTop level caught cv::Exception:\n" << e.what() << "\n!!!!!!!!!!!!!!\n" << std::endl; return 1;}
     catch(const std::exception& e) {std::cout << "\n!!!!!!!!!!!!!!\nTop level caught std::exception:\n" << e.what() << "\n!!!!!!!!!!!!!!\n" << std::endl; return 1;}
