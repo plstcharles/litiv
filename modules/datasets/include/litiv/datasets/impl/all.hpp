35de8352c705aa246118c60647af999fe1ae1ae8
Pierre-Luc St-Charles <pierre-luc.st-charles@polymtl.ca>
2016-09-23 18:49:38 -0400

    Add missing new datasets impl header grouper

---
+++ modules/datasets/include/litiv/datasets/impl/all.hpp
@@ -0,0 +1,29 @@
+
+// This file is part of the LITIV framework; visit the original repository at
+// https://github.com/plstcharles/litiv for more information.
+//
+// Copyright 2016 Pierre-Luc St-Charles; pierre-luc.st-charles<at>polymtl.ca
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#ifndef _LITIV_DATASETS_IMPL_H_
+#error "This file should never be included directly; use litiv/datasets.hpp instead"
+#else //_LITIV_DATASETS_IMPL_H_
+#include "litiv/datasets/impl/BSDS500.hpp"
+#include "litiv/datasets/impl/CDnet.hpp"
+//#include "litiv/datasets/impl/LITIV2012b.hpp"  @@@@ still need to work on interfaces for eDatasetType_VideoRegistr
+#include "litiv/datasets/impl/PETS2001.hpp"
+//#include "litiv/datasets/impl/vap-trimod2016.hpp" @@@ curr wip
+#include "litiv/datasets/impl/Wallflower.hpp"
+#endif //_LITIV_DATASETS_IMPL_H_
