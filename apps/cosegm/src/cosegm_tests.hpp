8116698d95f3448577b89e4b429caa908de0e5e7
Pierre-Luc St-Charles <pierre-luc.st-charles@polymtl.ca>
2016-10-17 14:44:37 -0400

    Add cosegms test dataset impl header

---
+++ apps/cosegm/src/cosegm_tests.hpp
@@ -0,0 +1,198 @@
+
+// This file is part of the LITIV framework; visit the original repository at
+// https://github.com/plstcharles/litiv for more information.
+//
+// Copyright 2016 Pierre-Luc St-Charles; pierre-luc.st-charles<at>polymtl.ca
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <opencv2/imgcodecs.hpp>
+#include "litiv/utils.hpp"
+#include "litiv/datasets.hpp"
+
+namespace lv {
+
+    static const DatasetList Dataset_CosegmTests = DatasetList(Dataset_Custom+1); // cheat; might cause problems if exposed in multiple external/custom specializations
+
+    struct ICosegmTestDataset {
+        virtual bool isUsingHalfGT() const = 0;
+    };
+
+    template<DatasetTaskList eDatasetTask, lv::ParallelAlgoType eEvalImpl>
+    struct Dataset_<eDatasetTask,Dataset_CosegmTests,eEvalImpl> :
+            public ICosegmTestDataset,
+            public IDataset_<eDatasetTask,DatasetSource_VideoArray,Dataset_CosegmTests,DatasetEval_BinaryClassifierArray,eEvalImpl> {
+    protected:
+        Dataset_(
+                const std::string& sOutputDirName, ///< output directory name for debug logs, evaluation reports and results archiving (will be created in VAP trimodal dataset results folder)
+                bool bSaveOutput=false, ///< defines whether results should be archived or not
+                bool bUseEvaluator=true, ///< defines whether results should be fully evaluated, or simply acknowledged
+                double dScaleFactor=1.0, ///< defines the scale factor to use to resize/rescale read packets
+                bool bUseHalfGT=1.0 ///< defines whether the input masks given should be half gt or not (useful for training)
+        ) :
+                IDataset_<eDatasetTask,DatasetSource_VideoArray,Dataset_CosegmTests,DatasetEval_BinaryClassifierArray,eEvalImpl>(
+                        "cosegm_tests",
+                        lv::datasets::getDatasetsRootPath()+"litiv/cosegm_tests/",
+                        lv::datasets::getDatasetsRootPath()+"litiv/cosegm_tests/results/"+lv::AddDirSlashIfMissing(sOutputDirName),
+                        "bin",
+                        ".png",
+                        std::vector<std::string>{"test01"},
+                        std::vector<std::string>(),
+                        std::vector<std::string>(),
+                        bSaveOutput,
+                        bUseEvaluator,
+                        false,
+                        dScaleFactor
+                ),m_bUsingHalfGT(bUseHalfGT) {}
+        virtual bool isUsingHalfGT() const override final {return m_bUsingHalfGT;}
+    protected:
+        const bool m_bUsingHalfGT;
+    };
+
+    template<DatasetTaskList eDatasetTask>
+    struct DataGroupHandler_<eDatasetTask,DatasetSource_VideoArray,Dataset_CosegmTests> :
+            public DataGroupHandler {
+    protected:
+        virtual void parseData() override {
+            this->m_vpBatches.clear();
+            this->m_bIsBare = true;
+            this->m_vpBatches.push_back(this->createWorkBatch(this->getName(),this->getRelativePath()));
+        }
+    };
+
+    template<DatasetTaskList eDatasetTask>
+    struct DataProducer_<eDatasetTask,DatasetSource_VideoArray,Dataset_CosegmTests> :
+            public IDataProducerWrapper_<eDatasetTask,DatasetSource_VideoArray,Dataset_CosegmTests> {
+        virtual size_t getInputStreamCount() const override final {
+            return 4; // 2x input images, 2x approx fg masks
+        }
+        virtual size_t getGTStreamCount() const override final {
+            return 2;
+        }
+        virtual bool isStreamGrayscale(size_t nStreamIdx) const override final {
+            return nStreamIdx!=0;
+        }
+    protected:
+        virtual void parseData() override final {
+            const ICosegmTestDataset& oDataset = dynamic_cast<const ICosegmTestDataset&>(*this->getRoot());
+            this->m_bUsingHalfGT = oDataset.isUsingHalfGT();
+            this->m_vvsInputPaths.clear();
+            this->m_vvsGTPaths.clear();
+            int nCurrIdx = 0;
+            while(true) {
+                std::vector<std::string> vCurrPaths = {
+                    cv::format("%simg%05da.png",this->getDataPath().c_str(),nCurrIdx),
+                    cv::format("%simg%05db.png",this->getDataPath().c_str(),nCurrIdx),
+                    cv::format("%smask%05da.png",this->getDataPath().c_str(),nCurrIdx),
+                    cv::format((this->m_bUsingHalfGT?"%sgt%05db.png":"%smask%05db.png"),this->getDataPath().c_str(),nCurrIdx),
+                };
+                const cv::Mat oInput0 = cv::imread(vCurrPaths[0]);
+                const cv::Mat oInput1 = cv::imread(vCurrPaths[1]);
+                const cv::Mat oMask0 = cv::imread(vCurrPaths[2],cv::IMREAD_GRAYSCALE);
+                const cv::Mat oMask1 = cv::imread(vCurrPaths[3],cv::IMREAD_GRAYSCALE);
+                if(!oInput0.empty() && !oInput1.empty() && !oMask0.empty() && !oMask1.empty()) {
+                    lvAssert(oInput0.size()==oInput1.size() && oMask0.size()==oMask1.size() && oInput0.size()==oMask0.size());
+                    this->m_vvsInputPaths.push_back(vCurrPaths);
+                    this->m_vvsGTPaths.push_back(
+                        std::vector<std::string>{
+                            cv::format("%sgt%05da.png",this->getDataPath().c_str(),nCurrIdx),
+                            cv::format("%sgt%05db.png",this->getDataPath().c_str(),nCurrIdx),
+                        }
+                    );
+                    const cv::Mat oGT0 = cv::imread(this->m_vvsGTPaths.back()[0],cv::IMREAD_GRAYSCALE);
+                    const cv::Mat oGT1 = cv::imread(this->m_vvsGTPaths.back()[1],cv::IMREAD_GRAYSCALE);
+                    lvAssert(!oGT0.empty() && !oGT1.empty() && oGT0.size()==oGT1.size() && oInput0.size()==oGT0.size());
+                    this->m_mGTIndexLUT[nCurrIdx] = nCurrIdx;
+                    ++nCurrIdx;
+                }
+                else
+                    break;
+            }
+            lvAssert(this->m_vvsInputPaths.size()>0 && this->m_vvsInputPaths[0].size()>0);
+            const cv::Size oOrigSize = cv::imread(this->m_vvsInputPaths[0][0]).size();
+            lvAssert(oOrigSize.area()>0);
+            const double dScale = this->getScaleFactor();
+            std::array<cv::Mat,2> aROIs = {
+                cv::imread(this->getDataPath()+"roi0.png",cv::IMREAD_GRAYSCALE),
+                cv::imread(this->getDataPath()+"roi1.png",cv::IMREAD_GRAYSCALE)
+            };
+            this->m_vInputROIs.resize(4);
+            this->m_vGTROIs.resize(2);
+            for(size_t a=0; a<2; ++a) {
+                if(aROIs[a].empty())
+                    aROIs[a] = cv::Mat(oOrigSize,CV_8UC1,cv::Scalar_<uchar>(255));
+                else
+                    lvAssert(aROIs[a].size()==oOrigSize);
+                if(dScale!=1.0)
+                    cv::resize(aROIs[a],aROIs[a],cv::Size(),dScale,dScale,cv::INTER_NEAREST);
+                this->m_vInputROIs[a] = aROIs[a].clone();
+                this->m_vInputROIs[a+2] = aROIs[a].clone();
+                this->m_vGTROIs[a] = aROIs[a].clone();
+            }
+            this->m_vInputSizes = std::vector<cv::Size>{aROIs[0].size(),aROIs[1].size(),aROIs[0].size(),aROIs[1].size()};
+            this->m_vGTSizes = std::vector<cv::Size>{aROIs[0].size(),aROIs[1].size()};
+            this->m_oMaxInputSize = this->m_oMaxGTSize = aROIs[0].size();
+        }
+
+        virtual cv::Mat getRawInput(size_t nPacketIdx) override final {
+            if(nPacketIdx>=this->m_vvsInputPaths.size())
+                return cv::Mat();
+            const std::vector<std::string>& vsInputPaths = this->m_vvsInputPaths[nPacketIdx];
+            const std::vector<cv::Size>& vsInputSizes = this->getInputSizeArray(nPacketIdx);
+            cv::Mat oPacket;
+            for(size_t nStreamIdx=0; nStreamIdx<vsInputPaths.size(); ++nStreamIdx) {
+                cv::Mat oCurrImg = cv::imread(vsInputPaths[nStreamIdx],this->isStreamGrayscale(nStreamIdx)?cv::IMREAD_GRAYSCALE:cv::IMREAD_COLOR);
+                if(oCurrImg.empty()) // if a single image is missing/cannot load, we skip the entire packet
+                    return cv::Mat();
+                const cv::Size& oPacketSize = vsInputSizes[nStreamIdx];
+                if(oCurrImg.size()!=oPacketSize)
+                    cv::resize(oCurrImg,oCurrImg,oPacketSize,0,0,cv::INTER_NEAREST);
+                if(oPacket.empty())
+                    oPacket = oCurrImg;
+                else {
+                    lvDbgAssert(oPacket.isContinuous() && oCurrImg.isContinuous());
+                    lvDbgAssert(size_t(oPacket.dataend-oPacket.datastart)==oPacket.total()*oPacket.elemSize());
+                    cv::Mat oNewPacket(int(oPacket.total()*oPacket.elemSize()+oCurrImg.total()*oCurrImg.elemSize()),1,CV_8UC1);
+                    std::copy(oPacket.datastart,oPacket.dataend,oNewPacket.data);
+                    std::copy(oCurrImg.datastart,oCurrImg.dataend,oNewPacket.data+uintptr_t(oPacket.dataend-oPacket.datastart));
+                    oPacket = oNewPacket;
+                }
+            }
+            return oPacket;
+        }
+
+        virtual void unpackInput(size_t nPacketIdx, std::vector<cv::Mat>& vUnpackedInput) override final {
+            const cv::Mat& oInput = this->getInput(nPacketIdx)/*.clone()*/;
+            const std::vector<cv::Size>& vSizes = this->getInputSizeArray(nPacketIdx);
+            if(oInput.empty()) {
+                for(size_t s=0; s<vSizes.size(); ++s)
+                    vUnpackedInput[s] = cv::Mat();
+                return;
+            }
+            lvAssert(oInput.isContinuous());
+            size_t nCurrPacketIdxOffset = 0;
+            const size_t nTotPacketSize = oInput.elemSize()*oInput.total();
+            for(size_t s=0; s<vSizes.size(); ++s) {
+                const size_t nCurrPacketSize = (s==0?3:1)*vSizes[s].area();
+                lvAssert_(nCurrPacketIdxOffset+nCurrPacketSize<=nTotPacketSize,"unpack out-of-bounds");
+                vUnpackedInput[s] = (nCurrPacketSize>0)?cv::Mat(vSizes[s],(s==0?CV_8UC3:CV_8UC1),(void*)(oInput.data+nCurrPacketIdxOffset)):cv::Mat();
+                nCurrPacketIdxOffset += nCurrPacketSize;
+            }
+            lvAssert_(nCurrPacketIdxOffset==nTotPacketSize,"unpack has leftover data");
+        }
+
+        bool m_bUsingHalfGT;
+    };
+
+} // namespace lv
--- apps/cosegm/src/main.cpp
+++ apps/cosegm/src/main.cpp
@@ -23,18 +23,38 @@
 #define EVALUATE_OUTPUT         0
 #define DISPLAY_OUTPUT          1
 ////////////////////////////////
-#define DATASET_ID              Dataset_VAPtrimod2016
+#define DATASET_VAPTRIMOD       0
+#define DATASET_MINI_TESTS      1
+////////////////////////////////
 #define DATASET_OUTPUT_PATH     "results_test"
 #define DATASET_PRECACHING      1
 #define DATASET_SCALE_FACTOR    1.0
 #define DATASET_WORKTHREADS     1
 ////////////////////////////////
+
+#if (DATASET_VAPTRIMOD+DATASET_MINI_TESTS/*+...*/)!=1
+#error "Must pick a single dataset."
+#endif //(DATASET_+.../*+...*/)!=1
+#if DATASET_VAPTRIMOD
+#define DATASET_ID Dataset_VAPtrimod2016
 #define DATASET_PARAMS \
-    DATASET_OUTPUT_PATH,                                         /* => const std::string& sOutputDirName */ \
-    bool(WRITE_IMG_OUTPUT),                                      /* => bool bSaveOutput */ \
-    bool(EVALUATE_OUTPUT),                                       /* => bool bUseEvaluator */ \
-    false,                                                       /* => bool bForce4ByteDataAlign */ \
-    DATASET_SCALE_FACTOR                                         /* => double dScaleFactor */
+    DATASET_OUTPUT_PATH,           /* => const std::string& sOutputDirName */ \
+    bool(WRITE_IMG_OUTPUT),        /* => bool bSaveOutput */ \
+    bool(EVALUATE_OUTPUT),         /* => bool bUseEvaluator */ \
+    false,                         /* => bool bForce4ByteDataAlign */ \
+    DATASET_SCALE_FACTOR,          /* => double dScaleFactor */ \
+    false,                         /* disable depth loading */ \
+    true                           /* enable undistort-on-load */
+#elif DATASET_MINI_TESTS
+#include "cosegm_tests.hpp"
+#define DATASET_ID Dataset_CosegmTests
+#define DATASET_PARAMS \
+    DATASET_OUTPUT_PATH,           /* => const std::string& sOutputDirName */ \
+    bool(WRITE_IMG_OUTPUT),        /* => bool bSaveOutput */ \
+    bool(EVALUATE_OUTPUT),         /* => bool bUseEvaluator */ \
+    DATASET_SCALE_FACTOR           /* => double dScaleFactor */
+//#elif DATASET_...
+#endif //DATASET_...
 
 void Analyze(std::string sWorkerName, lv::IDataHandlerPtr pBatch);
 using DatasetType = lv::Dataset_<lv::DatasetTask_Cosegm,lv::DATASET_ID,lv::NonParallel>;
@@ -42,7 +62,7 @@ using DatasetType = lv::Dataset_<lv::DatasetTask_Cosegm,lv::DATASET_ID,lv::NonPa
 int main(int, char**) {
     try {
         DatasetType::Ptr pDataset = DatasetType::create(DATASET_PARAMS);
-        lv::IDataHandlerPtrQueue vpBatches = pDataset->getSortedBatches(false);
+        lv::IDataHandlerPtrArray vpBatches = pDataset->getBatches(false);
         const size_t nTotPackets = pDataset->getInputCount();
         const size_t nTotBatches = vpBatches.size();
         if(nTotBatches==0 || nTotPackets==0)
@@ -51,16 +71,11 @@ int main(int, char**) {
         std::cout << "\n[" << lv::getTimeStamp() << "]\n" << std::endl;
         std::cout << "Executing algorithm with " << DATASET_WORKTHREADS << " thread(s)..." << std::endl;
         lv::WorkerPool<DATASET_WORKTHREADS> oPool;
-        std::queue<std::future<void>> vTaskResults;
-        while(!vpBatches.empty()) {
-            lv::IDataHandlerPtr pBatch = vpBatches.top();
-            vTaskResults.push(oPool.queueTask(Analyze,std::to_string(nTotBatches-vpBatches.size()+1)+"/"+std::to_string(nTotBatches),pBatch));
-            vpBatches.pop();
-        }
-        while(!vTaskResults.empty()) {
-            vTaskResults.front().get();
-            vTaskResults.pop();
-        }
+        std::vector<std::future<void>> vTaskResults;
+        for(lv::IDataHandlerPtr pBatch : vpBatches)
+            vTaskResults.push_back(oPool.queueTask(Analyze,std::to_string(nTotBatches-vpBatches.size()+1)+"/"+std::to_string(nTotBatches),pBatch));
+        for(std::future<void>& oTaskRes : vTaskResults)
+            oTaskRes.get();
         pDataset->writeEvalReport();
     }
     catch(const cv::Exception& e) {std::cout << "\n!!!!!!!!!!!!!!\nTop level caught cv::Exception:\n" << e.what() << "\n!!!!!!!!!!!!!!\n" << std::endl; return -1;}
@@ -76,8 +91,7 @@ void Analyze(std::string sWorkerName, lv::IDataHandlerPtr pBatch) {
     try {
         DatasetType::WorkBatch& oBatch = dynamic_cast<DatasetType::WorkBatch&>(*pBatch);
         lvAssert(oBatch.getInputPacketType()==lv::ImageArrayPacket && oBatch.getOutputPacketType()==lv::ImageArrayPacket);
-        lvAssert(oBatch.getOutputStreamCount()==oBatch.getInputStreamCount()); // app currently only supports 1:1 cosegm
-        lvAssert(oBatch.getFrameCount()>1);
+        lvAssert(oBatch.getOutputStreamCount()>=2 && oBatch.getInputStreamCount()>=2); // app works with stereo heads (min 2 images at once)
         if(DATASET_PRECACHING)
             oBatch.startPrecaching(EVALUATE_OUTPUT);
         const std::string sCurrBatchName = lv::clampString(oBatch.getName(),12);
@@ -86,9 +100,9 @@ void Analyze(std::string sWorkerName, lv::IDataHandlerPtr pBatch) {
         const std::vector<cv::Mat>& vROIs = oBatch.getFrameROIArray();
         lvAssert(!vROIs.empty() && vROIs.size()==oBatch.getInputStreamCount());
         size_t nCurrIdx = 0;
-        const std::vector<cv::Mat>& vInitInput = oBatch.getInputArray(nCurrIdx); // mat content becomes invalid on next getInput call
+        const std::vector<cv::Mat> vInitInput = oBatch.getInputArray(nCurrIdx); // mat content becomes invalid on next getInput call
         lvAssert(!vInitInput.empty() && vInitInput.size()==oBatch.getInputStreamCount());
-        std::vector<cv::Mat> vCurrFGMasks(vInitInput.size());
+        std::vector<cv::Mat> vCurrFGMasks(oBatch.getOutputStreamCount());
         //std::shared_ptr<IEdgeDetector> pAlgo = std::make_shared<CosegmenterType>();
         // init & defaults...
 #if DISPLAY_OUTPUT>0
@@ -97,11 +111,12 @@ void Analyze(std::string sWorkerName, lv::IDataHandlerPtr pBatch) {
         std::vector<std::vector<std::pair<cv::Mat,std::string>>> vvDisplayPairs;
         for(size_t nDisplayRowIdx=0; nDisplayRowIdx<vInitInput.size(); ++nDisplayRowIdx) {
             std::vector<std::pair<cv::Mat,std::string>> vRow(3);
-            vRow[0] = std::make_pair(vInitInput[nDisplayRowIdx].clone(),std::string()/*oBatch.getInputStreamName(nDisplayRowIdx)*/);
+            vRow[0] = std::make_pair(vInitInput[nDisplayRowIdx].clone(),oBatch.getInputStreamName(nDisplayRowIdx));
             vRow[1] = std::make_pair(cv::Mat(vInitInput[nDisplayRowIdx].size(),CV_8UC1,cv::Scalar_<uchar>(128)),"DEBUG");
             vRow[2] = std::make_pair(cv::Mat(vInitInput[nDisplayRowIdx].size(),CV_8UC1,cv::Scalar_<uchar>(128)),"OUTPUT");
             vvDisplayPairs.push_back(vRow);
         }
+        int nCurrTmpIdx = 0;
 #endif //DISPLAY_OUTPUT>0
         oBatch.startProcessing();
         while(nCurrIdx<nTotPacketCount) {
@@ -115,6 +130,14 @@ void Analyze(std::string sWorkerName, lv::IDataHandlerPtr pBatch) {
             lvDbgAssert(vCurrGT.size()==vCurrFGMasks.size());
 
             if(!vCurrGT[0].empty() && !vCurrGT[1].empty()) {
+
+                /*const std::string sTempOutPath = std::string(DATASET_OUTPUT_PATH)+"/tmp/";
+                cv::imwrite(cv::format("%simg%05da.png",sTempOutPath.c_str(),nCurrTmpIdx),vCurrInput[0]);
+                cv::imwrite(cv::format("%simg%05db.png",sTempOutPath.c_str(),nCurrTmpIdx),vCurrInput[1]);
+                cv::imwrite(cv::format("%sgt%05da.png",sTempOutPath.c_str(),nCurrTmpIdx),vCurrGT[0]);
+                cv::imwrite(cv::format("%sgt%05db.png",sTempOutPath.c_str(),nCurrTmpIdx),vCurrGT[1]);
+                cv::imwrite(sTempOutPath+"roia.png",vROIs[0]);
+                cv::imwrite(sTempOutPath+"roib.png",vROIs[1]);*/
 
                 cv::Mat test_rgb = vCurrInput[0].clone(), test_rgb_mask;
                 cv::bitwise_or(test_rgb/2,cv::Mat(vCurrInput[0].size(),CV_8UC3,cv::Scalar_<uchar>(127)),test_rgb_mask,vCurrGT[0]);
@@ -143,6 +166,8 @@ void Analyze(std::string sWorkerName, lv::IDataHandlerPtr pBatch) {
             const int nKeyPressed = pDisplayHelper->waitKey();
             if(nKeyPressed==(int)'q')
                 break;
+            else if(nKeyPressed==(int)'p')
+                ++nCurrTmpIdx;
 #endif //DISPLAY_OUTPUT>0
             oBatch.push(vCurrFGMasks,nCurrIdx++);
         }
