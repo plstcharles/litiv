20fc8a1828c88984980768fe00a7ed5ef19abf71
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2016-12-07 20:54:40 -0500

    Finish impl of l1dist regr/perf tests

--- modules/utils/test/distances.cpp
+++ modules/utils/test/distances.cpp
@@ -31,67 +31,81 @@ TEST(bittrick_signextshift,regression) {
 namespace {
 
     template<typename T>
-    std::unique_ptr<T[]> l1dist_signedarray(size_t n) {
+    std::unique_ptr<T[]> l1dist_genarray(size_t n, T min, T max) {
         std::random_device rd;
         std::mt19937 gen(rd());
-        std::uniform_real_distribution<T> uniform_dist(T(-10),T(10));
+        typedef std::conditional_t<std::is_integral<T>::value,std::uniform_int_distribution<int64_t>,std::uniform_real_distribution<double>> unif_distr;
+        unif_distr uniform_dist(min,max);
         std::unique_ptr<T[]> v(new T[n]);
         for(size_t i=0; i<n; ++i)
-            v[i] = uniform_dist(gen);
+            v[i] = (T)uniform_dist(gen);
         return std::move(v);
     }
 
     template<typename T>
     void l1dist_perftest(benchmark::State& st) {
         const size_t nArraySize = size_t(st.range(0));
-        std::unique_ptr<T[]> aVals = l1dist_signedarray<T>(nArraySize);
-        volatile T tLast = T(0);
-        volatile size_t nIdx = 0;
+        const size_t nLoopSize = size_t(st.range(1));
+        const T tMinVal = (T)st.range(2);
+        const T tMaxVal = (T)st.range(3);
+        std::unique_ptr<T[]> aVals = l1dist_genarray<T>(nArraySize,tMinVal,tMaxVal);
+        decltype(lv::L1dist(T(),T())) tLast = 0;
+        volatile size_t nArrayIdx = 0;
         while(st.KeepRunning()) {
-            ++nIdx %= nArraySize;
-            tLast = lv::L1dist(aVals[nIdx],tLast)/2;
+            for(size_t nLoopIdx=0; nLoopIdx<nLoopSize; ++nLoopIdx) {
+                ++nArrayIdx %= nArraySize;
+                tLast = lv::L1dist(aVals[nArrayIdx],(T)tLast)/2;
+            }
         }
         benchmark::DoNotOptimize(tLast);
     }
 
+}
+BENCHMARK_TEMPLATE(l1dist_perftest,double)->Args({1000000,100,-10000,10000})->Repetitions(10)->MinTime(1.0)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(l1dist_perftest,float)->Args({1000000,100,-1000,1000})->Repetitions(10)->MinTime(1.0)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(l1dist_perftest,short)->Args({1000000,100,-100,100})->Repetitions(10)->MinTime(1.0)->ReportAggregatesOnly(true);
+BENCHMARK_TEMPLATE(l1dist_perftest,char)->Args({1000000,100,-10,10})->Repetitions(10)->MinTime(1.0)->ReportAggregatesOnly(true);
+
+namespace {
     template<typename T>
-    struct l1dist_float_fixture : testing::Test {};
-    typedef testing::Types<float,double> l1dist_float_types;
+    struct l1dist_signed_fixture : testing::Test {};
+    typedef testing::Types<int8_t,int16_t,int32_t,float,double> l1dist_signed_types;
+}
+TYPED_TEST_CASE(l1dist_signed_fixture,l1dist_signed_types);
+TYPED_TEST(l1dist_signed_fixture,regression) {
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(0),TypeParam(0))),double(0));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(1),TypeParam(0))),double(1));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(0),TypeParam(1))),double(1));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(1),TypeParam(1))),double(0));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(-1),TypeParam(0))),double(1));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(0),TypeParam(-1))),double(1));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(-1),TypeParam(-1))),double(0));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(-10),TypeParam(10))),double(20));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(-20),TypeParam(0))),double(20));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(TypeParam(0),TypeParam(20))),double(20));
+    using TestTypeParam = std::conditional_t<std::is_same<double,TypeParam>::value,float,TypeParam>;
+    const TestTypeParam tMax = std::numeric_limits<TestTypeParam>::max();
+    const TestTypeParam tMin = std::numeric_limits<TestTypeParam>::min();
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(tMax,tMin)),(double(tMax)-tMin));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(tMin,tMax)),(double(tMax)-tMin));
 }
 
-TYPED_TEST_CASE(l1dist_float_fixture,l1dist_float_types);
-TYPED_TEST(l1dist_float_fixture,regression) {
+namespace {
+    template<typename T>
+    struct l1dist_unsigned_fixture : testing::Test {};
+    typedef testing::Types<uint8_t,uint16_t,uint32_t> l1dist_unsigned_types;
+}
+TYPED_TEST_CASE(l1dist_unsigned_fixture,l1dist_unsigned_types);
+TYPED_TEST(l1dist_unsigned_fixture,regression) {
     EXPECT_EQ((lv::L1dist(TypeParam(0),TypeParam(0))),TypeParam(0));
     EXPECT_EQ((lv::L1dist(TypeParam(1),TypeParam(0))),TypeParam(1));
     EXPECT_EQ((lv::L1dist(TypeParam(0),TypeParam(1))),TypeParam(1));
-    EXPECT_EQ((lv::L1dist(TypeParam(1),TypeParam(1))),TypeParam(0));
-    EXPECT_EQ((lv::L1dist(TypeParam(-1),TypeParam(0))),TypeParam(1));
-    EXPECT_EQ((lv::L1dist(TypeParam(0),TypeParam(-1))),TypeParam(1));
-    EXPECT_EQ((lv::L1dist(TypeParam(-1),TypeParam(-1))),TypeParam(0));
-    EXPECT_EQ((lv::L1dist(TypeParam(-10),TypeParam(10))),TypeParam(20));
-    EXPECT_EQ((lv::L1dist(TypeParam(-20),TypeParam(0))),TypeParam(20));
+    EXPECT_EQ((lv::L1dist(TypeParam(1),TypeParam(1))),TypeParam(0));;
     EXPECT_EQ((lv::L1dist(TypeParam(0),TypeParam(20))),TypeParam(20));
-}
-
-BENCHMARK_TEMPLATE(l1dist_perftest,float)->Arg(10)->Repetitions(5)->MinTime(1.0)->ReportAggregatesOnly(true);
-BENCHMARK_TEMPLATE(l1dist_perftest,double)->Arg(10)->Repetitions(5)->MinTime(1.0)->ReportAggregatesOnly(true);
-
-namespace {
-    template<typename T>
-    struct l1dist_int_fixture : testing::Test {};
-    typedef testing::Types<int8_t,int16_t/*,int32_t,int64_t*/> l1dist_int_types;
-}
-TYPED_TEST_CASE(l1dist_int_fixture,l1dist_int_types);
-TYPED_TEST(l1dist_int_fixture,regression) {
-    EXPECT_EQ((lv::L1dist(TypeParam(0),TypeParam(0))),size_t(0));
-}
-
-namespace {
-    template<typename T>
-    struct l1dist_uint_fixture : testing::Test {};
-    typedef testing::Types<uint8_t,uint16_t/*,uint32_t,uint64_t*/> l1dist_uint_types;
-}
-TYPED_TEST_CASE(l1dist_uint_fixture,l1dist_uint_types);
-TYPED_TEST(l1dist_uint_fixture,regression) {
-    EXPECT_EQ((lv::L1dist(TypeParam(0),TypeParam(0))),size_t(0));
+    EXPECT_EQ((lv::L1dist(TypeParam(20),TypeParam(0))),TypeParam(20));
+    EXPECT_EQ((lv::L1dist(TypeParam(20),TypeParam(20))),TypeParam(0));
+    const TypeParam tMax = std::numeric_limits<TypeParam>::max();
+    const TypeParam tMin = std::numeric_limits<TypeParam>::min();
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(tMax,tMin)),(double(tMax)-tMin));
+    EXPECT_DOUBLE_EQ(double(lv::L1dist(tMin,tMax)),(double(tMax)-tMin));
 }
