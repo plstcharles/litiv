5bcb35c986031c705a79fcd5965f1802bb814b1d
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2017-01-05 02:13:48 -0500

    Add barebone global tests for features2d module

---
+++ modules/features2d/test/global.cpp
@@ -0,0 +1,16 @@
+
+#include "litiv/features2d.hpp"
+#include "litiv/test.hpp"
+
+TEST(localDiff,regression) {
+    // ... @@@@ TODO
+}
+
+TEST(calcJointProbHist,regression) {
+
+    /*cv::Mat_<uchar> test1(2,2);
+    cv::Mat_<short> test2(2,2);
+    auto oHistData = lv::calcJointProbHist(std::make_tuple(test1,test2));*/
+
+    // ... @@@@ TODO
+}
