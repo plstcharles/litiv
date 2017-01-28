0402d7dfea462ad44ddc5f50baf209791bff5bd2
Pierre-Luc St-Charles <pierreluc.stcharles@gmail.com>
2017-01-28 14:21:52 -0500

    Fixup LBSP test valid zone bounds check

--- modules/features2d/test/lbsp.cpp
+++ modules/features2d/test/lbsp.cpp
@@ -26,27 +26,24 @@ TEST(lbsp,regression_compute) {
     const cv::Point2i oTargetPt(364,135);
     const int nPatchSize = 10;
     const cv::Size oWindowSize = pLBSP->windowSize();
-    const cv::Mat oInputCrop = oInput(cv::Rect(oTargetPt.x-oWindowSize.width/2-nPatchSize,oTargetPt.y-oWindowSize.height/2-nPatchSize,oWindowSize.width+2*nPatchSize,oWindowSize.height+2*nPatchSize)).clone();
+    const int nBorderSize = pLBSP->borderSize();
+    const cv::Mat oInputCrop = oInput(cv::Rect(oTargetPt.x-nBorderSize-nPatchSize,oTargetPt.y-nBorderSize-nPatchSize,oWindowSize.width+2*nPatchSize,oWindowSize.height+2*nPatchSize)).clone();
     cv::Mat oOutputDescMap1;
     pLBSP->compute2(oInputCrop,oOutputDescMap1);
     ASSERT_EQ(oInputCrop.size[0],oOutputDescMap1.size[0]);
     ASSERT_EQ(oInputCrop.size[1],oOutputDescMap1.size[1]);
     if(lv::checkIfExists(TEST_CURR_INPUT_DATA_ROOT "/test_lbsp.bin")) {
-        const cv::Rect oValidZone(oWindowSize.width/2,oWindowSize.height/2,oInputCrop.cols-oWindowSize.width/2,oInputCrop.rows-oWindowSize.height/2);
+        const cv::Rect oValidZone(nBorderSize,nBorderSize,oInputCrop.cols-nBorderSize*2,oInputCrop.rows-nBorderSize*2);
         const cv::Mat oOutputDescMap1_valid = oOutputDescMap1(oValidZone).clone();
         const cv::Mat oDescRefMap = cv::read(TEST_CURR_INPUT_DATA_ROOT "/test_lbsp.bin");
         const cv::Mat oDescRefMap_valid = oDescRefMap(oValidZone).clone();
-        for(int i=oWindowSize.height/2; i<oInputCrop.rows-oWindowSize.height/2; ++i)
-            for(int j=oWindowSize.width/2; j<oInputCrop.cols-oWindowSize.width/2; ++j)
-                for(int k=0; k<oInput.channels(); ++k)
-                    ASSERT_EQ(oOutputDescMap1.ptr<ushort>(i,j)[k],oDescRefMap.ptr<ushort>(i,j)[k]);
-        //ASSERT_TRUE(cv::isEqual<uchar>(oOutputDescMap1_valid,oDescRefMap_valid));
+        ASSERT_TRUE(cv::isEqual<uchar>(oOutputDescMap1_valid,oDescRefMap_valid));
     }
     else
         cv::write(TEST_CURR_INPUT_DATA_ROOT "/test_lbsp.bin",oOutputDescMap1);
     std::vector<cv::KeyPoint> vKeyPoints;
-    for(int nRowIdx=oWindowSize.height/2; nRowIdx<=oWindowSize.height/2+2*nPatchSize; ++nRowIdx)
-        for(int nColIdx=oWindowSize.width/2; nColIdx<=oWindowSize.width/2+2*nPatchSize; ++nColIdx)
+    for(int nRowIdx=nBorderSize; nRowIdx<=nBorderSize+2*nPatchSize; ++nRowIdx)
+        for(int nColIdx=nBorderSize; nColIdx<=nBorderSize+2*nPatchSize; ++nColIdx)
             vKeyPoints.emplace_back(cv::Point2f(float(nColIdx),float(nRowIdx)),float(std::max(oWindowSize.height,oWindowSize.width)));
     std::vector<cv::KeyPoint> vKeyPoints_modif = vKeyPoints;
     cv::Mat oOutputDescMap2;
@@ -54,24 +51,24 @@ TEST(lbsp,regression_compute) {
     ASSERT_EQ(oInputCrop.size[0],oOutputDescMap2.size[0]);
     ASSERT_EQ(oInputCrop.size[1],oOutputDescMap2.size[1]);
     ASSERT_EQ(vKeyPoints_modif.size(),vKeyPoints.size());
-    for(int nRowIdx=oWindowSize.height/2; nRowIdx<=oWindowSize.height/2+2*nPatchSize; ++nRowIdx)
-        for(int nColIdx=oWindowSize.width/2; nColIdx<=oWindowSize.width/2+2*nPatchSize; ++nColIdx)
+    for(int nRowIdx=nBorderSize; nRowIdx<=nBorderSize+2*nPatchSize; ++nRowIdx)
+        for(int nColIdx=nBorderSize; nColIdx<=nBorderSize+2*nPatchSize; ++nColIdx)
             for(int nChIdx=0; nChIdx<oInput.channels(); ++nChIdx)
                 ASSERT_EQ(oOutputDescMap1.ptr<ushort>(nRowIdx,nColIdx)[nChIdx],oOutputDescMap2.ptr<ushort>(nRowIdx,nColIdx)[nChIdx]);
     cv::Mat_<uchar> oDistMap;
     pLBSP->calcDistance(oOutputDescMap1,oOutputDescMap2,oDistMap);
     ASSERT_EQ(oDistMap.rows,oInputCrop.rows);
     ASSERT_EQ(oDistMap.cols,oInputCrop.cols);
-    for(int nRowIdx=oWindowSize.height/2; nRowIdx<=oWindowSize.height/2+2*nPatchSize; ++nRowIdx)
-        for(int nColIdx=oWindowSize.width/2; nColIdx<=oWindowSize.width/2+2*nPatchSize; ++nColIdx)
+    for(int nRowIdx=nBorderSize; nRowIdx<=nBorderSize+2*nPatchSize; ++nRowIdx)
+        for(int nColIdx=nBorderSize; nColIdx<=nBorderSize+2*nPatchSize; ++nColIdx)
             ASSERT_EQ(oDistMap.at<uchar>(nRowIdx,nColIdx),uchar(0));
     cv::Mat oOutputDescs;
     pLBSP->compute(oInputCrop,vKeyPoints_modif,oOutputDescs);
     ASSERT_EQ(vKeyPoints_modif.size(),vKeyPoints.size());
     ASSERT_EQ(size_t(oOutputDescs.rows),vKeyPoints.size());
     int nKeyPointIdx = 0;
-    for(int nRowIdx=oWindowSize.height/2; nRowIdx<=oWindowSize.height/2+2*nPatchSize; ++nRowIdx) {
-        for(int nColIdx=oWindowSize.width/2; nColIdx<=oWindowSize.width/2+2*nPatchSize; ++nColIdx) {
+    for(int nRowIdx=nBorderSize; nRowIdx<=nBorderSize+2*nPatchSize; ++nRowIdx) {
+        for(int nColIdx=nBorderSize; nColIdx<=nBorderSize+2*nPatchSize; ++nColIdx) {
             for(int nChIdx=0; nChIdx<oInput.channels(); ++nChIdx)
                 ASSERT_EQ(oOutputDescMap1.ptr<ushort>(nRowIdx,nColIdx)[nChIdx],oOutputDescs.ptr<ushort>(nKeyPointIdx)[nChIdx]);
             ++nKeyPointIdx;
